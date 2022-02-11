bc8085c1b8a2be0441933f0fa03a5967583a2574
Marcelo Henrique Neppel <marcelo.neppel@canonical.com>
2022-02-11 02:51:11 -0300

    Update tests

---
+++ tests/unit/helpers.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# Copyright 2021 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+from typing import Callable
+from unittest.mock import patch
+
+
+def patch_network_get(private_address="10.1.157.116") -> Callable:
+    def network_get(*args, **kwargs) -> dict:
+        """Patch for the not-yet-implemented testing backend needed for `bind_address`.
+
+        This patch decorator can be used for cases such as:
+        self.model.get_binding(event.relation).network.bind_address
+        """
+        return {
+            "bind-addresses": [
+                {
+                    "addresses": [{"value": private_address}],
+                }
+            ]
+        }
+
+    return patch("ops.testing._TestingModelBackend.network_get", network_get)
