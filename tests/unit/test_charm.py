e09ca9c1afa519f30bed45d3201b4709e22262a6
Marcelo Henrique Neppel <marcelo.neppel@canonical.com>
2022-02-11 02:51:11 -0300

    Update tests

--- tests/unit/test_charm.py
+++ tests/unit/test_charm.py
@@ -5,13 +5,18 @@ import re
 import unittest
 from unittest.mock import Mock, patch
 
-from ops.model import ActiveStatus, WaitingStatus
+from helpers import patch_network_get
+from lightkube import codecs
+from lightkube.resources.core_v1 import Pod
+from ops.model import ActiveStatus, BlockedStatus, WaitingStatus
 from ops.testing import Harness
+from tenacity import RetryError
 
 from charm import PostgresqlOperatorCharm
 
 
 class TestCharm(unittest.TestCase):
+    @patch_network_get(private_address="1.1.1.1")
     def setUp(self):
         self._peer_relation = "postgresql-replicas"
         self._postgresql_container = "postgresql"
@@ -21,25 +26,55 @@ class TestCharm(unittest.TestCase):
         self.addCleanup(self.harness.cleanup)
         self.harness.begin()
         self.charm = self.harness.charm
+        self._context = {
+            "namespace": self.harness.model.name,
+            "app_name": self.harness.model.app.name,
+        }
+
+    @patch("charm.Patroni.render_postgresql_conf_file")
+    @patch("charm.PostgresqlOperatorCharm._patch_pod_labels")
+    @patch("charm.PostgresqlOperatorCharm._create_resources")
+    def test_on_install(
+        self,
+        _create_resources,
+        _patch_pod_labels,
+        _render_postgresql_conf_file,
+    ):
+        self.charm.on.install.emit()
+        _create_resources.assert_called_once()
+        _patch_pod_labels.assert_called_once()
+        _render_postgresql_conf_file.assert_called_once()
 
     def test_on_leader_elected(self):
         # Assert that there is no password in the peer relation.
         self.harness.add_relation(self._peer_relation, self.charm.app.name)
         self.assertIsNone(self.charm._peers.data[self.charm.app].get("postgres-password", None))
+        self.assertIsNone(self.charm._peers.data[self.charm.app].get("replication-password", None))
 
         # Check that a new password was generated on leader election.
         self.harness.set_leader()
-        password = self.charm._peers.data[self.charm.app].get("postgres-password", None)
-        self.assertIsNotNone(password)
+        superuser_password = self.charm._peers.data[self.charm.app].get("postgres-password", None)
+        self.assertIsNotNone(superuser_password)
+
+        replication_password = self.charm._peers.data[self.charm.app].get(
+            "replication-password", None
+        )
+        self.assertIsNotNone(replication_password)
 
         # Trigger a new leader election and check that the password is still the same.
         self.harness.set_leader(False)
         self.harness.set_leader()
         self.assertEqual(
-            self.charm._peers.data[self.charm.app].get("postgres-password", None), password
+            self.charm._peers.data[self.charm.app].get("postgres-password", None),
+            superuser_password,
+        )
+        self.assertEqual(
+            self.charm._peers.data[self.charm.app].get("replication-password", None),
+            replication_password,
         )
 
-    def test_on_postgresql_pebble_ready(self):
+    @patch("charm.Patroni.render_patroni_yml_file")
+    def test_on_postgresql_pebble_ready(self, _render_patroni_yml_file):
         # Check that the initial plan is empty.
         plan = self.harness.get_container_pebble_plan(self._postgresql_container)
         self.assertEqual(plan.to_dict(), {})
@@ -53,6 +88,7 @@ class TestCharm(unittest.TestCase):
                 self.harness.model.unit.status,
                 WaitingStatus("waiting for Pebble in workload container"),
             )
+            _render_patroni_yml_file.assert_not_called()
 
         # Get the current and the expected layer from the pebble plan and the _postgresql_layer
         # method, respectively.
@@ -66,6 +102,7 @@ class TestCharm(unittest.TestCase):
         self.assertEqual(self.harness.model.unit.status, ActiveStatus())
         container = self.harness.model.unit.get_container(self._postgresql_container)
         self.assertEqual(container.get_service(self._postgresql_service).is_running(), True)
+        _render_patroni_yml_file.assert_called_once()
 
     @patch("charm.PostgresqlOperatorCharm._get_postgres_password")
     def test_on_get_postgres_password(self, _get_postgres_password):
@@ -75,28 +112,136 @@ class TestCharm(unittest.TestCase):
         _get_postgres_password.assert_called_once()
         mock_event.set_results.assert_called_once_with({"postgres-password": "test-password"})
 
+    @patch("charm.Patroni.get_primary")
+    def test_on_get_primary(self, _get_primary):
+        mock_event = Mock()
+        _get_primary.return_value = "postgresql-k8s-1"
+        self.charm._on_get_primary(mock_event)
+        _get_primary.assert_called_once()
+        mock_event.set_results.assert_called_once_with({"primary": "postgresql-k8s-1"})
+
+    @patch("charm.Patroni.get_primary")
+    def test_fail_to_get_primary(self, _get_primary):
+        mock_event = Mock()
+        _get_primary.side_effect = [RetryError("fake error")]
+        self.charm._on_get_primary(mock_event)
+        _get_primary.assert_called_once()
+        mock_event.set_results.assert_not_called()
+
+    @patch("ops.model.Container.start")
+    @patch("charm.sleep")
+    @patch("ops.model.Container.stop")
+    def test_restart_postgresql_service(self, _stop, _sleep, _start):
+        self.charm._restart_postgresql_service()
+        _stop.assert_called_once_with(self._postgresql_service)
+        _sleep.assert_called_once_with(30)
+        _start.assert_called_once_with(self._postgresql_service)
+        self.assertEqual(
+            self.harness.model.unit.status,
+            ActiveStatus(),
+        )
+
+    @patch("ops.model.Container.start")
+    @patch("charm.sleep")
+    @patch("ops.model.Container.stop")
+    def test_restart_postgresql_service_without_waiting(self, _stop, _sleep, _start):
+        self.charm._restart_postgresql_service(wait=False)
+        _stop.assert_called_once_with(self._postgresql_service)
+        _sleep.assert_not_called()
+        _start.assert_called_once_with(self._postgresql_service)
+        self.assertEqual(
+            self.harness.model.unit.status,
+            ActiveStatus(),
+        )
+
+    @patch("charm.PostgresqlOperatorCharm._restart_postgresql_service")
+    @patch("charm.Patroni.get_postgresql_state")
+    def test_on_update_status(self, _get_postgresql_state, _restart_postgresql_service):
+        _get_postgresql_state.side_effect = ["running", "restarting", "stopping"]
+
+        # Test running status.
+        self.charm.on.update_status.emit()
+        _restart_postgresql_service.assert_not_called()
+
+        # Test restarting status.
+        self.charm.on.update_status.emit()
+        _restart_postgresql_service.assert_called_once_with(wait=False)
+
+        # Test stopping status.
+        _restart_postgresql_service.reset_mock()
+        self.charm.on.update_status.emit()
+        _restart_postgresql_service.assert_called_once_with()
+
+    @patch("charm.PostgresqlOperatorCharm._restart_postgresql_service")
+    @patch("charm.Patroni.get_postgresql_state")
+    def test_on_update_status_with_error_on_postgresql_status_check(
+        self, _get_postgresql_state, _restart_postgresql_service
+    ):
+        _get_postgresql_state.side_effect = [RetryError("fake error")]
+        self.charm.on.update_status.emit()
+        _restart_postgresql_service.assert_not_called()
+        self.assertEqual(
+            self.harness.model.unit.status,
+            BlockedStatus("failed to check PostgreSQL state with error RetryError[fake error]"),
+        )
+
+    @patch("charm.PostgresqlOperatorCharm._patch_pod_labels")
+    def test_on_upgrade_charm(self, _patch_pod_labels):
+        self.charm.on.upgrade_charm.emit()
+        _patch_pod_labels.assert_called_once()
+
+    @patch("charm.Client")
+    def test_create_resources(self, _client):
+        self.charm._create_resources()
+        with open("src/resources.yaml") as f:
+            for obj in codecs.load_all_yaml(f, context=self._context):
+                _client.return_value.create.assert_any_call(obj)
+
+    @patch("charm.Client")
+    def test_patch_pod_labels(self, _client):
+        self.charm._patch_pod_labels()
+        expected_patch = {
+            "metadata": {
+                "labels": {"application": "patroni", "cluster-name": self.charm._namespace}
+            }
+        }
+        _client.return_value.patch.assert_called_once_with(
+            Pod,
+            name=self.charm._unit.replace("/", "-"),
+            namespace=self.charm._namespace,
+            obj=expected_patch,
+        )
+
     def test_postgresql_layer(self):
         # Test with the already generated password.
         self.harness.add_relation(self._peer_relation, self.charm.app.name)
         self.harness.set_leader()
         plan = self.charm._postgresql_layer().to_dict()
         expected = {
-            "summary": "postgresql layer",
-            "description": "pebble config layer for postgresql",
+            "summary": "postgresql + patroni layer",
+            "description": "pebble config layer for postgresql + patroni",
             "services": {
                 self._postgresql_service: {
                     "override": "replace",
-                    "summary": "entrypoint of the postgresql image",
-                    "command": "/usr/local/bin/docker-entrypoint.sh postgres",
+                    "summary": "entrypoint of the postgresql + patroni image",
+                    "command": "/usr/bin/python3 /usr/local/bin/patroni /var/lib/postgresql/data/patroni.yml",
                     "startup": "enabled",
+                    "user": "postgres",
+                    "group": "postgres",
                     "environment": {
-                        "PGDATA": "/var/lib/postgresql/data/pgdata",
-                        "POSTGRES_PASSWORD": self.charm._get_postgres_password(),
+                        "PATRONI_KUBERNETES_LABELS": "{application: patroni, cluster-name: postgresql-k8s}",
+                        "PATRONI_KUBERNETES_NAMESPACE": self.charm._namespace,
+                        "PATRONI_NAME": "postgresql-k8s-0",
+                        "PATRONI_SCOPE": self.charm._namespace,
+                        "PATRONI_REPLICATION_USERNAME": "replication",
+                        "PATRONI_REPLICATION_PASSWORD": self.charm._get_replication_password(),
+                        "PATRONI_SUPERUSER_USERNAME": "postgres",
+                        "PATRONI_SUPERUSER_PASSWORD": self.charm._get_postgres_password(),
                     },
                 }
             },
         }
-        self.assertEqual(plan, expected)
+        self.assertDictEqual(plan, expected)
 
     def test_new_password(self):
         # Test the password generation twice in order to check if we get different passwords and
@@ -113,6 +258,17 @@ class TestCharm(unittest.TestCase):
         # Test for a None password.
         self.harness.add_relation(self._peer_relation, self.charm.app.name)
         self.assertIsNone(self.charm._get_postgres_password())
+
+        # Then test for a non empty password after leader election and peer data set.
+        self.harness.set_leader()
+        password = self.charm._get_postgres_password()
+        self.assertIsNotNone(password)
+        self.assertNotEqual(password, "")
+
+    def test_get_replication_password(self):
+        # Test for a None password.
+        self.harness.add_relation(self._peer_relation, self.charm.app.name)
+        self.assertIsNone(self.charm._get_replication_password())
 
         # Then test for a non empty password after leader election and peer data set.
         self.harness.set_leader()
---
+++ tests/unit/test_patroni.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python3
+# Copyright 2021 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+import unittest
+from unittest.mock import mock_open, patch
+
+from patroni import STORAGE_PATH, Patroni
+
+
+class TestPatroni(unittest.TestCase):
+    def setUp(self):
+        # Setup Patroni wrapper.
+        self.patroni = Patroni("1.1.1.1")
+
+    @patch("requests.get")
+    def test_get_postgresql_state(self, _get):
+        _get.return_value.json.return_value = {"state": "running"}
+        state = self.patroni.get_postgresql_state()
+        self.assertEqual(state, "running")
+        _get.assert_called_once_with("http://1.1.1.1:8008/health")
+
+    @patch("requests.get")
+    def test_get_primary(self, _get):
+        # Mock Patroni cluster API.
+        _get.return_value.json.return_value = {
+            "members": [
+                {"name": "postgresql-k8s-0", "role": "replica"},
+                {"name": "postgresql-k8s-1", "role": "leader"},
+                {"name": "postgresql-k8s-2", "role": "replica"},
+            ]
+        }
+
+        # Test returning pod name.
+        primary = self.patroni.get_primary()
+        self.assertEqual(primary, "postgresql-k8s-1")
+        _get.assert_called_once_with("http://1.1.1.1:8008/cluster")
+
+        # Test returning unit name.
+        _get.reset_mock()
+        primary = self.patroni.get_primary(unit_name_pattern=True)
+        self.assertEqual(primary, "postgresql-k8s/1")
+        _get.assert_called_once_with("http://1.1.1.1:8008/cluster")
+
+    @patch("os.chmod")
+    @patch("os.chown")
+    @patch("pwd.getpwnam")
+    @patch("tempfile.NamedTemporaryFile")
+    def test_render_file(self, _temp_file, _pwnam, _chown, _chmod):
+        # Set a mocked temporary filename.
+        filename = "/tmp/temporaryfilename"
+        _temp_file.return_value.name = filename
+        # Setup a mock for the `open` method.
+        mock = mock_open()
+        # Patch the `open` method with our mock.
+        with patch("builtins.open", mock, create=True):
+            # Set the uid/gid return values for lookup of 'postgres' user.
+            _pwnam.return_value.pw_uid = 35
+            _pwnam.return_value.pw_gid = 35
+            # Call the method using a temporary configuration file.
+            self.patroni._render_file(filename, "rendered-content", 0o640)
+
+        # Check the rendered file is opened with "w+" mode.
+        self.assertEqual(mock.call_args_list[0][0], (filename, "w+"))
+        # Ensure that the correct user is lookup up.
+        _pwnam.assert_called_with("postgres")
+        # Ensure the file is chmod'd correctly.
+        _chmod.assert_called_with(filename, 0o640)
+        # Ensure the file is chown'd correctly.
+        _chown.assert_called_with(filename, uid=35, gid=35)
+
+    @patch("charm.Patroni._render_file")
+    def test_render_patroni_yml_file(self, _render_file):
+        # Get the expected content from a file.
+        with open("tests/data/patroni.yml") as file:
+            expected_content = file.read()
+
+        # Setup a mock for the `open` method, set returned data to postgresql.conf template.
+        with open("templates/patroni.yml.j2", "r") as f:
+            mock = mock_open(read_data=f.read())
+
+        # Patch the `open` method with our mock.
+        with patch("builtins.open", mock, create=True):
+            # Call the method
+            self.patroni.render_patroni_yml_file()
+
+        # Check the template is opened read-only in the call to open.
+        self.assertEqual(mock.call_args_list[0][0], ("templates/patroni.yml.j2", "r"))
+        # Ensure the correct rendered template is sent to _render_file method.
+        _render_file.assert_called_once_with(
+            f"{STORAGE_PATH}/patroni.yml",
+            expected_content,
+            0o644,
+        )
+
+    @patch("charm.Patroni._render_file")
+    def test_render_postgresql_conf_file(self, _render_file):
+        # Get the expected content from a file.
+        with open("tests/data/postgresql.conf") as file:
+            expected_content = file.read()
+
+        # Setup a mock for the `open` method, set returned data to postgresql.conf template.
+        with open("templates/postgresql.conf.j2", "r") as f:
+            mock = mock_open(read_data=f.read())
+
+        # Patch the `open` method with our mock.
+        with patch("builtins.open", mock, create=True):
+            # Call the method
+            self.patroni.render_postgresql_conf_file()
+
+        # Check the template is opened read-only in the call to open.
+        self.assertEqual(mock.call_args_list[0][0], ("templates/postgresql.conf.j2", "r"))
+        # Ensure the correct rendered template is sent to _render_file method.
+        _render_file.assert_called_once_with(
+            f"{STORAGE_PATH}/postgresql-k8s-operator.conf",
+            expected_content,
+            0o644,
+        )
