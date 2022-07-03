9f897d95c68f995da614e5fdcf08a5490491965f
Marcelo Henrique Neppel <marcelo.neppel@canonical.com>
2022-07-03 13:43:08 -0300

    Add more database units on test

--- tests/integration/test_db.py
+++ tests/integration/test_db.py
@@ -17,6 +17,8 @@ logger = logging.getLogger(__name__)
 
 FINOS_WALTZ_APP_NAME = "finos-waltz"
 ANOTHER_FINOS_WALTZ_APP_NAME = "another-finos-waltz"
+APPLICATION_UNITS = 1
+DATABASE_UNITS = 3
 
 
 async def test_finos_waltz_db(ops_test: OpsTest) -> None:
@@ -25,58 +27,71 @@ async def test_finos_waltz_db(ops_test: OpsTest) -> None:
     Args:
         ops_test: The ops test framework
     """
-    # Build and deploy the PostgreSQL charm.
-    charm = await ops_test.build_charm(".")
-    resources = {
-        "postgresql-image": METADATA["resources"]["postgresql-image"]["upstream-source"],
-    }
-    await ops_test.model.deploy(
-        charm, resources=resources, application_name=DATABASE_APP_NAME, trust=True  # , num_units=3
-    ),
-    # Wait until the PostgreSQL charm is successfully deployed.
-    await ops_test.model.wait_for_idle(
-        apps=[DATABASE_APP_NAME],
-        status="active",
-        raise_on_blocked=True,
-        timeout=1000,
-        # wait_for_exact_units=3,
-    )
-    # assert len(ops_test.model.applications[DATABASE_APP_NAME].units) == 3
+    async with ops_test.fast_forward():
+        # Build and deploy the PostgreSQL charm.
+        charm = await ops_test.build_charm(".")
+        resources = {
+            "postgresql-image": METADATA["resources"]["postgresql-image"]["upstream-source"],
+        }
+        await ops_test.model.deploy(
+            charm,
+            resources=resources,
+            application_name=DATABASE_APP_NAME,
+            trust=True,
+            num_units=DATABASE_UNITS,
+        ),
+        # Wait until the PostgreSQL charm is successfully deployed.
+        await ops_test.model.wait_for_idle(
+            apps=[DATABASE_APP_NAME],
+            status="active",
+            raise_on_blocked=True,
+            timeout=1000,
+            wait_for_exact_units=DATABASE_UNITS,
+        )
+        assert len(ops_test.model.applications[DATABASE_APP_NAME].units) == DATABASE_UNITS
 
-    for unit in ops_test.model.applications[DATABASE_APP_NAME].units:
-        assert unit.workload_status == "active"
+        for unit in ops_test.model.applications[DATABASE_APP_NAME].units:
+            assert unit.workload_status == "active"
 
-    # Deploy and test the first deployment of Finos Waltz.
-    relation_id = await deploy_and_relate_application_with_postgresql(
-        ops_test, "finos-waltz-k8s", FINOS_WALTZ_APP_NAME, 1, channel="edge"
-    )
-    await check_database_creation(ops_test, "waltz")
+        # Deploy and test the first deployment of Finos Waltz.
+        relation_id = await deploy_and_relate_application_with_postgresql(
+            ops_test, "finos-waltz-k8s", FINOS_WALTZ_APP_NAME, APPLICATION_UNITS, channel="edge"
+        )
+        await check_database_creation(ops_test, "waltz")
 
-    finos_waltz_users = [f"relation_id_{relation_id}"]
+        finos_waltz_users = [f"relation_id_{relation_id}"]
 
-    await check_database_users_existence(ops_test, finos_waltz_users, [])
+        await check_database_users_existence(ops_test, finos_waltz_users, [])
 
-    # Deploy and test another deployment of Finos Waltz.
-    another_relation_id = await deploy_and_relate_application_with_postgresql(
-        ops_test, "finos-waltz-k8s", ANOTHER_FINOS_WALTZ_APP_NAME, 1, channel="edge"
-    )
-    await check_database_creation(ops_test, "waltz")
+        # Deploy and test another deployment of Finos Waltz.
+        another_relation_id = await deploy_and_relate_application_with_postgresql(
+            ops_test,
+            "finos-waltz-k8s",
+            ANOTHER_FINOS_WALTZ_APP_NAME,
+            APPLICATION_UNITS,
+            channel="edge",
+        )
+        await check_database_creation(ops_test, "waltz")
 
-    another_finos_waltz_users = [f"relation_id_{another_relation_id}"]
+        another_finos_waltz_users = [f"relation_id_{another_relation_id}"]
 
-    await check_database_users_existence(
-        ops_test, finos_waltz_users + another_finos_waltz_users, []
-    )
+        await check_database_users_existence(
+            ops_test, finos_waltz_users + another_finos_waltz_users, []
+        )
 
-    # Scale down the second deployment of Finos Waltz and confirm that the first deployment
-    # is still active.
-    await ops_test.model.remove_application(ANOTHER_FINOS_WALTZ_APP_NAME, block_until_done=True)
+        # Scale down the second deployment of Finos Waltz and confirm that the first deployment
+        # is still active.
+        await ops_test.model.remove_application(
+            ANOTHER_FINOS_WALTZ_APP_NAME, block_until_done=True
+        )
 
-    another_finos_waltz_users = []
-    await check_database_users_existence(ops_test, finos_waltz_users, another_finos_waltz_users)
+        another_finos_waltz_users = []
+        await check_database_users_existence(
+            ops_test, finos_waltz_users, another_finos_waltz_users
+        )
 
-    # Remove the first deployment of Finos Waltz.
-    await ops_test.model.remove_application(FINOS_WALTZ_APP_NAME, block_until_done=True)
+        # Remove the first deployment of Finos Waltz.
+        await ops_test.model.remove_application(FINOS_WALTZ_APP_NAME, block_until_done=True)
 
-    # Remove the PostgreSQL application.
-    await ops_test.model.remove_application(DATABASE_APP_NAME, block_until_done=True)
+        # Remove the PostgreSQL application.
+        await ops_test.model.remove_application(DATABASE_APP_NAME, block_until_done=True)
