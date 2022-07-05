c90531992c3fea53edaf0a9496d837e3c6506bb6
Marcelo Henrique Neppel <marcelo.neppel@canonical.com>
2022-07-05 09:42:53 -0300

    Comment tests

--- src/constants.py
+++
@@ -1,7 +0,0 @@
-# Copyright 2022 Canonical Ltd.
-# See LICENSE file for licensing details.
-
-"""File containing constants to be used in the charm."""
-
-LEGACY_DB = "db"
-LEGACY_DB_ADMIN = "db-admin"
