7f8d300b7b25a0f9d65e044c3af3311c2cce4d4b
Marcelo Henrique Neppel <marcelo.neppel@canonical.com>
2022-07-06 14:28:23 -0300

    Merge

--- tests/unit/test_charm.py
+++ tests/unit/test_charm.py
@@ -1,7 +1,6 @@
 # Copyright 2021 Canonical Ltd.
 # See LICENSE file for licensing details.
 
-import re
 import unittest
 from unittest.mock import Mock, call, patch
 
@@ -276,17 +275,6 @@ class TestCharm(unittest.TestCase):
             },
         }
         self.assertDictEqual(plan, expected)
-
-    def test_new_password(self):
-        # Test the password generation twice in order to check if we get different passwords and
-        # that they meet the required criteria.
-        first_password = self.charm._new_password()
-        self.assertEqual(len(first_password), 16)
-        self.assertIsNotNone(re.fullmatch("[a-zA-Z0-9\b]{16}$", first_password))
-
-        second_password = self.charm._new_password()
-        self.assertIsNotNone(re.fullmatch("[a-zA-Z0-9\b]{16}$", second_password))
-        self.assertNotEqual(second_password, first_password)
 
     @patch("charm.PostgresqlOperatorCharm._patch_pod_labels")
     @patch("charm.PostgresqlOperatorCharm._create_resources")
