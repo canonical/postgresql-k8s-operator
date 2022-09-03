5951cb09fbd5e6cdfaeedbc7b38a27deaf40cdd8
Marcelo Henrique Neppel <marcelo.neppel@canonical.com>
2022-09-03 16:24:35 -0300

    Update test

--- tests/integration/test_db_with_tls.py
+++ tests/integration/test_db_with_tls.py
@@ -7,8 +7,6 @@ from pytest_operator.plugin import OpsTest
 from tests.helpers import METADATA
 from tests.integration.helpers import (
     DATABASE_APP_NAME,
-    TLS_RESOURCES,
-    attach_resource,
     check_database_creation,
     check_database_users_existence,
     deploy_and_relate_application_with_postgresql,
@@ -16,11 +14,31 @@ from tests.integration.helpers import (
 )
 
 MATTERMOST_APP_NAME = "mattermost"
+TLS_CERTIFICATES_APP_NAME = "tls-certificates-operator"
 APPLICATION_UNITS = 2
-DATABASE_UNITS = 3
+DATABASE_UNITS = 1
 
 
-@pytest.mark.db_relation
+@pytest.mark.abort_on_fail
+@pytest.mark.tls_tests
+@pytest.mark.skip_if_deployed
+async def test_deploy_active(ops_test: OpsTest):
+    """Build the charm and deploy it."""
+    charm = await ops_test.build_charm(".")
+    async with ops_test.fast_forward():
+        await ops_test.model.deploy(
+            charm,
+            resources={
+                "postgresql-image": METADATA["resources"]["postgresql-image"]["upstream-source"]
+            },
+            application_name=DATABASE_APP_NAME,
+            num_units=DATABASE_UNITS,
+            trust=True,
+        )
+        await ops_test.model.wait_for_idle(apps=[DATABASE_APP_NAME], status="active", timeout=1000)
+
+
+@pytest.mark.tls_tests
 async def test_mattermost_db(ops_test: OpsTest) -> None:
     """Deploy Mattermost to test the 'db' relation.
 
@@ -30,48 +48,20 @@ async def test_mattermost_db(ops_test: OpsTest) -> None:
         ops_test: The ops test framework
     """
     async with ops_test.fast_forward():
-        # Build and deploy the PostgreSQL charm.
-        charm = await ops_test.build_charm(".")
-        resources = {
-            "postgresql-image": METADATA["resources"]["postgresql-image"]["upstream-source"],
-            "cert-file": METADATA["resources"]["cert-file"]["filename"],
-            "key-file": METADATA["resources"]["key-file"]["filename"],
-        }
-        await ops_test.model.deploy(
-            charm,
-            resources=resources,
-            application_name=DATABASE_APP_NAME,
-            trust=True,
-            num_units=DATABASE_UNITS,
-        ),
-        # Wait until the PostgreSQL charm is successfully deployed.
+        # Deploy TLS Certificates operator.
+        config = {"generate-self-signed-certificates": "true", "ca-common-name": "Test CA"}
+        await ops_test.model.deploy(TLS_CERTIFICATES_APP_NAME, channel="edge", config=config)
         await ops_test.model.wait_for_idle(
-            apps=[DATABASE_APP_NAME],
-            status="active",
-            raise_on_blocked=True,
-            timeout=1000,
-            wait_for_exact_units=DATABASE_UNITS,
+            apps=[TLS_CERTIFICATES_APP_NAME], status="active", timeout=1000
         )
-        assert len(ops_test.model.applications[DATABASE_APP_NAME].units) == DATABASE_UNITS
 
-        # Add TLS certificate and key to PostgreSQL.
-        for rsc_name, src_path in TLS_RESOURCES.items():
-            await attach_resource(ops_test, DATABASE_APP_NAME, rsc_name, src_path)
+        # Relate it to the PostgreSQL to enable TLS.
+        await ops_test.model.relate(DATABASE_APP_NAME, TLS_CERTIFICATES_APP_NAME)
+        await ops_test.model.wait_for_idle(status="active", timeout=1000)
 
-        # Wait for all units enabling TLS (after the execution of the upgrade charm hook).
+        # Wait for all units enabling TLS.
         for unit in ops_test.model.applications[DATABASE_APP_NAME].units:
             assert await is_tls_enabled(ops_test, unit.name)
-
-        await ops_test.model.wait_for_idle(
-            apps=[DATABASE_APP_NAME],
-            status="active",
-            raise_on_blocked=False,  # The charm can be blocked when PostgreSQL is not ready yet.
-            timeout=1000,
-            wait_for_exact_units=DATABASE_UNITS,
-        )
-
-        for unit in ops_test.model.applications[DATABASE_APP_NAME].units:
-            assert unit.workload_status == "active"
 
         # Deploy and test Mattermost.
         relation_id = await deploy_and_relate_application_with_postgresql(
@@ -82,9 +72,3 @@ async def test_mattermost_db(ops_test: OpsTest) -> None:
         mattermost_users = [f"relation_id_{relation_id}"]
 
         await check_database_users_existence(ops_test, mattermost_users, [])
-
-        # Remove the deployment of Mattermost.
-        await ops_test.model.remove_application(MATTERMOST_APP_NAME, block_until_done=True)
-
-        # Remove the PostgreSQL application.
-        await ops_test.model.remove_application(DATABASE_APP_NAME, block_until_done=True)
