ae83ade675c60a3647937c82aa8d4e6321bd7833
Marcelo Henrique Neppel <marcelo.neppel@canonical.com>
2022-09-03 15:45:15 -0300

    Add one more exception handler

--- tests/integration/ha_tests/application-charm/src/continuous_writes.py
+++ tests/integration/ha_tests/application-charm/src/continuous_writes.py
@@ -2,96 +2,47 @@
 # See LICENSE file for licensing details.
 
 """This file is meant to run in the background continuously writing entries to PostgreSQL."""
-import signal
 import sys
 
 import psycopg2 as psycopg2
 
-interrupt_loop = False
-
 
 def continuous_writes(connection_string: str, starting_number: int):
-    # Define a handler for signals.
-    def interrupt_handler(_, __):
-        global interrupt_loop
-        interrupt_loop = True
-
-    signal.signal(signal.SIGINT, interrupt_handler)  # Handle ctrl+c.
-
     write_value = starting_number
 
-    global interrupt_loop
-    connection = None
-    reconnect = False
-    while not interrupt_loop:
+    try:
+        with psycopg2.connect(connection_string) as connection, connection.cursor() as cursor:
+            connection.autocommit = True
+            cursor.execute("CREATE TABLE IF NOT EXISTS continuous_writes(number INTEGER);")
+            cursor.execute(
+                "CREATE UNIQUE INDEX IF NOT EXISTS number ON continuous_writes(number);"
+            )
+    finally:
+        connection.close()
+
+    while True:
         try:
-            # Connect to the database and create the table that will store the inserted rows.
-            if reconnect:
-                connection.close()
-            if connection is None or reconnect:
-                connection = psycopg2.connect(connection_string)
+            with psycopg2.connect(connection_string) as connection, connection.cursor() as cursor:
                 connection.autocommit = True
-                reconnect = False
-                with connection, connection.cursor() as cursor:
-                    cursor.execute("CREATE TABLE IF NOT EXISTS continuous_writes(number INTEGER);")
-                    cursor.execute(
-                        "CREATE UNIQUE INDEX IF NOT EXISTS number ON continuous_writes(number);"
-                    )
-            with connection, connection.cursor() as cursor:
                 cursor.execute(f"INSERT INTO continuous_writes(number) VALUES({write_value});")
-        except psycopg2.errors.ConnectionException as e:
-            # this means that the primary was not able to be found. An application should try to
-            # reconnect and re-write the previous value. Hence, we `continue` here, without
-            # incrementing `write_value` as to try to insert this value again.
-            reconnect = True
-            f = open("/tmp/demofile1.txt", "a")
+        except psycopg2.OperationalError as e:
+            # We should not raise this exception but instead increment the write value and move
+            # on, indicating that there was a failure writing to the database.
+            # psycopg2.InterfaceError
+            # psycopg2.OperationalError
+            f = open("/tmp/demofile3.txt", "a")
             f.write(str(type(e)))
+            f.write("\n")
+            f.write(str(e.pgcode))
+            f.write("\n")
+            f.write(str(e.pgerror))
+            f.write("\n")
+            f.write(str(e))
+            f.write("\n")
+            f.write(f"{write_value}")
+            f.write("\n")
             f.close()
             continue
-        # except psycopg2.InterfaceError as e:
-        #     # We should not raise this exception but instead increment the write value and move
-        #     # on, indicating that there was a failure writing to the database.
-        #     # psycopg2.InterfaceError
-        #     # psycopg2.OperationalError
-        #     f = open("/tmp/demofile7.txt", "a")
-        #     f.write(str(type(e)))
-        #     f.write("\n")
-        #     f.write(str(e.pgcode))
-        #     f.write("\n")
-        #     f.write(str(e.pgerror))
-        #     f.write("\n")
-        #     f.write(str(e))
-        #     f.write("\n")
-        #     f.write(f"{write_value}")
-        #     f.write("\n")
-        #     f.close()
-        #     reconnect = True
-        #     continue
-        # except psycopg2.OperationalError as e:
-        #     # We should not raise this exception but instead increment the write value and move
-        #     # on, indicating that there was a failure writing to the database.
-        #     # psycopg2.InterfaceError
-        #     # psycopg2.OperationalError
-        #     f = open("/tmp/demofile4.txt", "a")
-        #     f.write(str(type(e)))
-        #     f.write("\n")
-        #     f.write(str(e.pgcode))
-        #     f.write("\n")
-        #     f.write(str(psycopg2.errors.UniqueViolation))
-        #     f.write("\n")
-        #     f.write(str(e.pgerror))
-        #     f.write("\n")
-        #     f.write(str(dir(e)))
-        #     f.write("\n")
-        #     f.write(str(e))
-        #     f.write("\n")
-        #     f.write(f"{write_value}")
-        #     f.write("\n")
-        #     f.close()
-        #     reconnect = True
-        #     continue
-        #     # if e.pgcode != psycopg2.errors.UniqueViolation:
-        #     #     continue
         except psycopg2.errors.UniqueViolation as e:
             # We should not raise this exception but instead increment the write value and move
             # on, indicating that there was a failure writing to the database.
@@ -126,18 +77,10 @@ def continuous_writes(connection_string: str, starting_number: int):
             f.write(f"{write_value}")
             f.write("\n")
             f.close()
-            reconnect = True
-            continue
-        except Exception as e:
-            f = open("/tmp/demofile3.txt", "a")
-            f.write(str(type(e)))
-            f.close()
-            pass
+        finally:
+            connection.close()
 
         write_value += 1
-
-    connection.close()
-    print("connection closed")
 
 
 def main():
