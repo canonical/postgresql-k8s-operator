f0da31ca6db0b2493160cdf4dfd9c4bc29a64fd1
Marcelo Henrique Neppel <marcelo.neppel@canonical.com>
2022-10-06 16:04:23 -0300

    Add initial test

---
+++ tests/integration/ha_tests/helpers.py
@@ -0,0 +1,281 @@
+# Copyright 2022 Canonical Ltd.
+# See LICENSE file for licensing details.
+import asyncio
+import subprocess
+from pathlib import Path
+from typing import Optional
+
+import psycopg2
+import requests
+import yaml
+from kubernetes import config
+from kubernetes.client.api import core_v1_api
+from kubernetes.stream import stream
+from pytest_operator.plugin import OpsTest
+from tenacity import (
+    RetryError,
+    Retrying,
+    stop_after_attempt,
+    stop_after_delay,
+    wait_fixed,
+)
+
+from tests.integration.helpers import get_unit_address
+
+METADATA = yaml.safe_load(Path("./metadata.yaml").read_text())
+PORT = 5432
+
+
+class ProcessError(Exception):
+    pass
+
+
+async def app_name(ops_test: OpsTest, application_name: str = "postgresql-k8s") -> Optional[str]:
+    """Returns the name of the cluster running PostgreSQL.
+
+    This is important since not all deployments of the PostgreSQL charm have the application name
+    "postgresql-k8s".
+
+    Note: if multiple clusters are running PostgreSQL this will return the one first found.
+    """
+    status = await ops_test.model.get_status()
+    for app in ops_test.model.applications:
+        if application_name in status["applications"][app]["charm"]:
+            return app
+
+    return None
+
+
+async def change_master_start_timeout(ops_test: OpsTest, seconds: Optional[int]) -> None:
+    """Change master start timeout configuration.
+
+    Args:
+        ops_test: ops_test instance.
+        seconds: number of seconds to set in master_start_timeout configuration.
+    """
+    for attempt in Retrying(stop=stop_after_delay(30 * 2), wait=wait_fixed(3)):
+        with attempt:
+            app = await app_name(ops_test)
+            primary_name = await get_primary(ops_test, app)
+            unit_ip = await get_unit_address(ops_test, primary_name)
+            requests.patch(
+                f"http://{unit_ip}:8008/config",
+                json={"master_start_timeout": seconds},
+            )
+
+
+async def count_writes(ops_test: OpsTest) -> int:
+    """Count the number of writes in the database."""
+    app = await app_name(ops_test)
+    password = await get_password(ops_test, app)
+    status = await ops_test.model.get_status()
+    try:
+        for attempt in Retrying(
+            stop=stop_after_attempt(len(status["applications"][app]["units"]))
+        ):
+            with attempt:
+                host = list(status["applications"][app]["units"].values())[
+                    attempt.retry_state.attempt_number - 1
+                ]["address"]
+                connection_string = (
+                    f"dbname='application' user='operator'"
+                    f" host='{host}' password='{password}' connect_timeout=10"
+                )
+                with psycopg2.connect(
+                    connection_string
+                ) as connection, connection.cursor() as cursor:
+                    cursor.execute("SELECT COUNT(number) FROM continuous_writes;")
+                    count = cursor.fetchone()[0]
+                connection.close()
+    except RetryError:
+        return -1
+    return count
+
+
+async def cut_network_from_unit(ops_test: OpsTest, unit_name: str) -> None:
+    """Cut network from a k8s pod.
+    Args:
+        ops_test: ops_test instance.
+        unit_name: the name of the unit to cut network from.
+    """
+    # Add an iptables rule to block
+    unit_ip = await get_unit_address(ops_test, unit_name)
+    cut_network_command = f"sudo iptables -I INPUT -s {unit_ip} -j DROP"
+    subprocess.check_call(cut_network_command.split())
+    cut_network_command = f"sudo iptables -I OUTPUT -s {unit_ip} -j DROP"
+    subprocess.check_call(cut_network_command.split())
+
+
+async def get_master_start_timeout(ops_test: OpsTest) -> Optional[int]:
+    """Get the master start timeout configuration.
+
+    Args:
+        ops_test: ops_test instance.
+
+    Returns:
+        master start timeout in seconds or None if it's using the default value.
+    """
+    for attempt in Retrying(stop=stop_after_delay(30 * 2), wait=wait_fixed(3)):
+        with attempt:
+            app = await app_name(ops_test)
+            primary_name = await get_primary(ops_test, app)
+            unit_ip = await get_unit_address(ops_test, primary_name)
+            configuration_info = requests.get(f"http://{unit_ip}:8008/config")
+            master_start_timeout = configuration_info.json().get("master_start_timeout")
+            return int(master_start_timeout) if master_start_timeout is not None else None
+
+
+async def get_password(ops_test: OpsTest, app) -> str:
+    """Use the charm action to retrieve the password from provided application.
+
+    Returns:
+        string with the password stored on the peer relation databag.
+    """
+    # Can retrieve from any unit running unit, so we pick the first.
+    for attempt in Retrying(stop=stop_after_attempt(len(ops_test.model.applications[app].units))):
+        with attempt:
+            unit_name = (
+                ops_test.model.applications[app].units[attempt.retry_state.attempt_number - 1].name
+            )
+            action = await ops_test.model.units.get(unit_name).run_action("get-password")
+            action = await asyncio.wait_for(action.wait(), 10)
+            return action.results["operator-password"]
+
+
+async def get_primary(ops_test: OpsTest, app) -> str:
+    """Use the charm action to retrieve the primary from provided application.
+
+    Returns:
+        string with the password stored on the peer relation databag.
+    """
+    # Can retrieve from any unit running unit, so we pick the first.
+    unit_name = ops_test.model.applications[app].units[0].name
+    action = await ops_test.model.units.get(unit_name).run_action("get-primary")
+    action = await action.wait()
+    return action.results["primary"]
+
+
+async def is_unit_reachable_from(
+    ops_test: OpsTest, origin_unit: str, target_unit: str, use_controller_namespace: bool = False
+) -> bool:
+    """Test network reachability between hosts.
+
+    Args:
+        origin_machine: hostname of the machine to test connection from
+        target_machine: hostname of the machine to test connection to
+    """
+    try:
+        pod_name = origin_unit.replace("/", "-")
+        target_pod_endpoint = f'{target_unit.replace("/", "-")}.{target_unit.split("/")[0]}-endpoints.{ops_test.model.info.name}'
+        namespace = (
+            f"controller-{ops_test.controller_name}"
+            if use_controller_namespace
+            else ops_test.model.info.name
+        )
+        command = (
+            f"curl {target_pod_endpoint}:8008 --connect-timeout 5"
+            if use_controller_namespace
+            else f"curl {target_pod_endpoint}:8008 --connect-timeout 5"
+        )
+        container = "--container api-server" if use_controller_namespace else ""
+        print(f"namespace: {namespace}")
+        complete_command = f"kubectl exec -n {namespace} pod/{pod_name} {container} -- {command}"
+        print(f"command: {complete_command}")
+        subprocess.check_call(complete_command.split())
+        return True
+    except subprocess.CalledProcessError as e:
+        print(str(e))
+        return False
+
+
+async def postgresql_ready(ops_test, unit_name: str) -> bool:
+    """Verifies a PostgreSQL instance is running and available."""
+    unit_ip = await get_unit_address(ops_test, unit_name)
+    try:
+        for attempt in Retrying(stop=stop_after_delay(60 * 5), wait=wait_fixed(3)):
+            with attempt:
+                instance_health_info = requests.get(f"http://{unit_ip}:8008/health")
+                assert instance_health_info.status_code == 200
+    except RetryError:
+        return False
+
+    return True
+
+
+async def restore_network_for_unit(ops_test: OpsTest, unit_name: str) -> None:
+    """Restore network for a k8s pod.
+    Args:
+        ops_test: ops_test instance.
+        unit_name: the name of the unit to cut network from.
+    """
+    # Remove the previously added iptables rule.
+    unit_ip = await get_unit_address(ops_test, unit_name)
+    restore_network_command = f"sudo iptables -D INPUT -s {unit_ip} -j DROP"
+    subprocess.check_call(restore_network_command.split())
+    restore_network_command = f"sudo iptables -D OUTPUT -s {unit_ip} -j DROP"
+    subprocess.check_call(restore_network_command.split())
+
+
+async def secondary_up_to_date(ops_test: OpsTest, unit_name: str, expected_writes: int) -> bool:
+    """Checks if secondary is up-to-date with the cluster.
+
+    Retries over the period of one minute to give secondary adequate time to copy over data.
+    """
+    app = await app_name(ops_test)
+    password = await get_password(ops_test, app)
+    status = await ops_test.model.get_status()
+    host = status["applications"][app]["units"][unit_name]["address"]
+    connection_string = (
+        f"dbname='application' user='operator'"
+        f" host='{host}' password='{password}' connect_timeout=10"
+    )
+
+    try:
+        for attempt in Retrying(stop=stop_after_delay(60), wait=wait_fixed(3)):
+            with attempt:
+                with psycopg2.connect(
+                    connection_string
+                ) as connection, connection.cursor() as cursor:
+                    cursor.execute("SELECT COUNT(number) FROM continuous_writes;")
+                    secondary_writes = cursor.fetchone()[0]
+                    assert secondary_writes == expected_writes
+    except RetryError:
+        return False
+    finally:
+        connection.close()
+
+    return True
+
+
+async def start_continuous_writes(ops_test: OpsTest, app: str) -> None:
+    """Start continuous writes to PostgreSQL."""
+    # Start the process by relating the application to the database or
+    # by calling the action if the relation already exists.
+    relations = [
+        relation
+        for relation in ops_test.model.applications[app].relations
+        if not relation.is_peer
+        and f"{relation.requires.application_name}:{relation.requires.name}"
+        == "application:database"
+    ]
+    if not relations:
+        await ops_test.model.relate(app, "application")
+        await ops_test.model.wait_for_idle(status="active", timeout=1000)
+    else:
+        action = await ops_test.model.units.get("application/0").run_action(
+            "start-continuous-writes"
+        )
+        await action.wait()
+
+
+async def stop_continuous_writes(ops_test: OpsTest) -> int:
+    """Stops continuous writes to PostgreSQL and returns the last written value."""
+    action = await ops_test.model.units.get("application/0").run_action("stop-continuous-writes")
+    action = await action.wait()
+    return int(action.results["writes"])
+
+
+def wait_network_restore(ops_test: OpsTest, unit_name: str):
+    for attempt in Retrying():
+        with attempt:
+            pass
---
+++ tests/integration/ha_tests/test_self_healing.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python3
+# Copyright 2022 Canonical Ltd.
+# See LICENSE file for licensing details.
+from time import sleep
+
+import pytest
+from pytest_operator.plugin import OpsTest
+from tenacity import Retrying, stop_after_delay, wait_fixed
+
+from tests.integration.ha_tests.helpers import (
+    METADATA,
+    app_name,
+    count_writes,
+    cut_network_from_unit,
+    get_primary,
+    is_unit_reachable_from,
+    postgresql_ready,
+    restore_network_for_unit,
+    secondary_up_to_date,
+    start_continuous_writes,
+    stop_continuous_writes,
+)
+
+PATRONI_PROCESS = "/usr/local/bin/patroni"
+POSTGRESQL_PROCESS = "postgres"
+DB_PROCESSES = [POSTGRESQL_PROCESS, PATRONI_PROCESS]
+
+
+@pytest.mark.abort_on_fail
+@pytest.mark.ha_self_healing_tests
+async def test_build_and_deploy(ops_test: OpsTest) -> None:
+    """Build and deploy three unit of PostgreSQL."""
+    # It is possible for users to provide their own cluster for HA testing. Hence, check if there
+    # is a pre-existing cluster.
+    if await app_name(ops_test):
+        return
+
+    charm = await ops_test.build_charm(".")
+    async with ops_test.fast_forward():
+        await ops_test.model.deploy(
+            charm,
+            resources={
+                "postgresql-image": METADATA["resources"]["postgresql-image"]["upstream-source"]
+            },
+            num_units=3,
+            trust=True,
+        )
+        await ops_test.model.wait_for_idle(status="active", timeout=1000)
+
+
+# @pytest.mark.ha_self_healing_tests
+# @pytest.mark.parametrize("process", /DB_PROCESSES)
+async def test_freeze_db_process(
+    ops_test: OpsTest, process: str, continuous_writes, master_start_timeout
+) -> None:
+    # Locate primary unit.
+    app = await app_name(ops_test)
+    primary_name = await get_primary(ops_test, app)
+
+    # Start an application that continuously writes data to the database.
+    await start_continuous_writes(ops_test, app)
+
+    # Freeze the database process.
+    await send_signal_to_process(ops_test, primary_name, process, "SIGSTOP")
+
+    async with ops_test.fast_forward():
+        # Verify new writes are continuing by counting the number of writes before and after a
+        # 2 minutes wait (a db process freeze takes more time to trigger a fail-over).
+        writes = await count_writes(ops_test)
+        for attempt in Retrying(stop=stop_after_delay(60 * 2), wait=wait_fixed(3)):
+            with attempt:
+                more_writes = await count_writes(ops_test)
+                assert more_writes > writes, "writes not continuing to DB"
+
+        # Un-freeze the old primary.
+        await send_signal_to_process(ops_test, primary_name, process, "SIGCONT")
+
+        # Verify that the database service got restarted and is ready in the old primary.
+        assert await postgresql_ready(ops_test, primary_name)
+
+    # Verify that a new primary gets elected (ie old primary is secondary).
+    new_primary_name = await get_primary(ops_test, app)
+    assert new_primary_name != primary_name
+
+    # Verify that no writes to the database were missed after stopping the writes.
+    total_expected_writes = await stop_continuous_writes(ops_test)
+    for attempt in Retrying(stop=stop_after_delay(60), wait=wait_fixed(3)):
+        with attempt:
+            actual_writes = await count_writes(ops_test)
+            assert total_expected_writes == actual_writes, "writes to the db were missed."
+
+    # Verify that old primary is up-to-date.
+    assert await secondary_up_to_date(
+        ops_test, primary_name, total_expected_writes
+    ), "secondary not up to date with the cluster after restarting."
+
+
+@pytest.mark.ha_self_healing_tests
+async def test_network_cut(ops_test: OpsTest, continuous_writes, master_start_timeout):
+    """Completely cut and restore network."""
+    # Locate primary unit.
+    app = await app_name(ops_test)
+    primary_name = await get_primary(ops_test, app)
+
+    await start_continuous_writes(ops_test, app)
+
+    await cut_network_from_unit(ops_test, primary_name)
+
+    sleep(10)
+
+    # verify machine is not reachable from peer units
+    unit_names = [unit.name for unit in ops_test.model.applications[app].units]
+    for unit_name in set(unit_names) - {primary_name}:
+        assert not await is_unit_reachable_from(
+            ops_test, unit_name, primary_name
+        ), "❌ unit is reachable from peer"
+
+    # verify machine is not reachable from controller
+    assert not await is_unit_reachable_from(
+        ops_test, "controller-0", primary_name, use_controller_namespace=True
+    ), "❌ unit is reachable from controller"
+
+    # verify that connection is not possible
+    # assert not await is_connection_possible(ops_test, primary_name), "❌ Connection is possible after network cut"
+
+    await restore_network_for_unit(ops_test, primary_name)
+
+    # wait until network is reestablished for the unit
+    # await wait_network_restore(ops_test, primary_name)
+
+    # # verify that connection is possible
+    # assert await is_connection_possible(ops_test, primary_name), "❌ Connection is not possible after network restore"
+    #
+    # # verify instance is part of the cluster
+    # assert await is_unit_in_cluster(
+    #     ops_test, primary_unit.name, another_unit.name
+    # ), "Unit not online in the cluster"
