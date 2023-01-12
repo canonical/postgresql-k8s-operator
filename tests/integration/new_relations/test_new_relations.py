da808628f7592d4feafba0c4e174c06b5b74fcde
Marcelo Henrique Neppel <marcelo.neppel@canonical.com>
2023-01-12 11:09:12 -0300

    Fix delete user call when user was not created before (#75)
    * Fix delete user call when user was not created before
    
    * Add integration test for the fix

--- tests/integration/new_relations/test_new_relations.py
+++ tests/integration/new_relations/test_new_relations.py
@@ -28,6 +28,7 @@ FIRST_DATABASE_RELATION_NAME = "first-database"
 SECOND_DATABASE_RELATION_NAME = "second-database"
 MULTIPLE_DATABASE_CLUSTERS_RELATION_NAME = "multiple-database-clusters"
 ALIASED_MULTIPLE_DATABASE_CLUSTERS_RELATION_NAME = "aliased-multiple-database-clusters"
+NO_DATABASE_RELATION_NAME = "no-database"
 
 
 @pytest.mark.abort_on_fail
@@ -358,3 +359,20 @@ async def test_restablish_relation(ops_test: OpsTest):
         cursor.execute("SELECT data FROM test;")
         data = cursor.fetchone()
         assert data[0] == "other data"
+
+
+@pytest.mark.database_relation_tests
+async def test_relation_with_no_database_name(ops_test: OpsTest):
+    """Test that a relation with no database name doesn't block the charm."""
+    async with ops_test.fast_forward():
+        # Relate the charms using a relation that doesn't provide a database name.
+        await ops_test.model.add_relation(
+            f"{APPLICATION_APP_NAME}:{NO_DATABASE_RELATION_NAME}", DATABASE_APP_NAME
+        )
+        await ops_test.model.wait_for_idle(apps=APP_NAMES, status="active", raise_on_blocked=True)
+
+        # Break the relation.
+        await ops_test.model.applications[DATABASE_APP_NAME].remove_relation(
+            f"{DATABASE_APP_NAME}", f"{APPLICATION_APP_NAME}:{NO_DATABASE_RELATION_NAME}"
+        )
+        await ops_test.model.wait_for_idle(apps=APP_NAMES, status="active", raise_on_blocked=True)
