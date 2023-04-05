63cb1c2f11b51be06304be4e581601db68ec7f51
Marcelo Henrique Neppel <marcelo.neppel@canonical.com>
2023-04-05 17:23:55 -0300

    Fix freeze db test

--- tests/integration/ha_tests/helpers.py
+++ tests/integration/ha_tests/helpers.py
@@ -1,7 +1,7 @@
 # Copyright 2022 Canonical Ltd.
 # See LICENSE file for licensing details.
 from pathlib import Path
-from typing import Optional
+from typing import Dict, Optional
 
 import psycopg2
 import requests
@@ -37,6 +37,11 @@ class ProcessError(Exception):
     """Raised when a process fails."""
 
 
+def get_patroni_cluster(unit_ip: str) -> Dict[str, str]:
+    resp = requests.get(f"http://{unit_ip}:8008/cluster")
+    return resp.json()
+
+
 async def change_master_start_timeout(ops_test: OpsTest, seconds: Optional[int]) -> None:
     """Change master start timeout configuration.
 
@@ -55,6 +60,14 @@ async def change_master_start_timeout(ops_test: OpsTest, seconds: Optional[int])
             )
 
 
+async def check_writes(ops_test) -> int:
+    """Gets the total writes from the test charm and compares to the writes from db."""
+    total_expected_writes = await stop_continuous_writes(ops_test)
+    actual_writes = await count_writes(ops_test)
+    assert total_expected_writes == actual_writes, "writes to the db were missed."
+    return total_expected_writes
+
+
 async def count_writes(ops_test: OpsTest, down_unit: str = None) -> int:
     """Count the number of writes in the database."""
     app = await app_name(ops_test)
@@ -62,23 +75,31 @@ async def count_writes(ops_test: OpsTest, down_unit: str = None) -> int:
     status = await ops_test.model.get_status()
     for unit_name, unit in status["applications"][app]["units"].items():
         if unit_name != down_unit:
-            host = unit["address"]
+            cluster = get_patroni_cluster(unit["address"])
             break
+    for member in cluster["members"]:
+        if member["role"] != "replica" and member["host"].split(".")[0] != (
+            down_unit or ""
+        ).replace("/", "-"):
+            host = member["host"]
+
+    # Translate the service hostname to an IP address.
+    print(host)
+    model = ops_test.model.info
+    client = Client(namespace=model.name)
+    service = client.get(Pod, name=host.split(".")[0])
+    ip = service.status.podIP
+
     connection_string = (
         f"dbname='application' user='operator'"
-        f" host='{host}' password='{password}' connect_timeout=10"
+        f" host='{ip}' password='{password}' connect_timeout=10"
     )
-    try:
-        for attempt in Retrying(stop=stop_after_delay(60), wait=wait_fixed(3)):
-            with attempt:
-                with psycopg2.connect(
-                    connection_string
-                ) as connection, connection.cursor() as cursor:
-                    cursor.execute("SELECT COUNT(number) FROM continuous_writes;")
-                    count = cursor.fetchone()[0]
-                connection.close()
-    except RetryError:
-        return -1
+
+    with psycopg2.connect(connection_string) as connection, connection.cursor() as cursor:
+        cursor.execute("SELECT COUNT(number) FROM continuous_writes;")
+        count = cursor.fetchone()[0]
+    connection.close()
+    print(count)
     return count
 
 
@@ -153,7 +174,7 @@ async def is_replica(ops_test: OpsTest, unit_name: str) -> bool:
 
                 # A member that restarted has the DB process stopped may
                 # take some time to know that a new primary was elected.
-                if role == "replica":
+                if role != "leader":
                     return True
                 else:
                     raise MemberNotUpdatedOnClusterError()
@@ -227,6 +248,19 @@ async def send_signal_to_process(
         await ops_test.model.applications[app].add_unit(count=1)
         await ops_test.model.wait_for_idle(apps=[app], status="active", timeout=1000)
 
+    # if signal == "SIGCONT":
+    #     command = f"ssh {unit_name} pkill --signal {signal} -f {process}"
+    #     return_code, _, _ = await ops_test.juju(*command.split())
+    #
+    #     if return_code != 0:
+    #         raise ProcessError(
+    #             "Expected command %s to succeed instead it failed: %s",
+    #             command,
+    #             return_code,
+    #         )
+    #
+    #     return
+
     # Load Kubernetes configuration to connect to the cluster.
     config.load_kube_config()
 
@@ -254,6 +288,38 @@ async def send_signal_to_process(
             command,
             response.returncode,
         )
+
+
+async def start_continuous_writes(ops_test: OpsTest, app: str) -> None:
+    """Start continuous writes to PostgreSQL."""
+    # Start the process by relating the application to the database or
+    # by calling the action if the relation already exists.
+    relations = [
+        relation
+        for relation in ops_test.model.applications[app].relations
+        if not relation.is_peer
+        and f"{relation.requires.application_name}:{relation.requires.name}"
+        == "application:database"
+    ]
+    if not relations:
+        await ops_test.model.relate(app, "application")
+        await ops_test.model.wait_for_idle(status="active", timeout=1000)
+    else:
+        action = (
+            await ops_test.model.applications["application"]
+            .units[0]
+            .run_action("start-continuous-writes")
+        )
+        await action.wait()
+    for attempt in Retrying(stop=stop_after_delay(60 * 5), wait=wait_fixed(3), reraise=True):
+        with attempt:
+            action = (
+                await ops_test.model.applications["application"]
+                .units[0]
+                .run_action("start-continuous-writes")
+            )
+            await action.wait()
+            assert action.results["result"] == "True", "Unable to create continuous_writes table"
 
 
 async def stop_continuous_writes(ops_test: OpsTest) -> int:
