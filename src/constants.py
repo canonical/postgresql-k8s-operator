3d8ca9a1820b8dd7864b2e810f5a054dd7631c8c
Paulo Machado <paulo.machado@canonical.com>
2023-05-23 18:10:50 -0300

    initial implementation
    * missing log files paths

--- src/constants.py
+++ src/constants.py
@@ -10,6 +10,8 @@ REPLICATION_USER = "replication"
 REPLICATION_PASSWORD_KEY = "replication-password"
 REWIND_USER = "rewind"
 REWIND_PASSWORD_KEY = "rewind-password"
+MONITORING_USER = "monitoring"
+MONITORING_PASSWORD_KEY = "monitoring-password"
 TLS_KEY_FILE = "key.pem"
 TLS_CA_FILE = "ca.pem"
 TLS_CERT_FILE = "cert.pem"
@@ -17,5 +19,7 @@ USER = "operator"
 USER_PASSWORD_KEY = "operator-password"
 WORKLOAD_OS_GROUP = "postgres"
 WORKLOAD_OS_USER = "postgres"
+METRICS_PORT = "9187"
+POSTGRES_LOG_FILES = []
 # List of system usernames needed for correct work of the charm/workload.
 SYSTEM_USERS = [BACKUP_USER, REPLICATION_USER, REWIND_USER, USER]
