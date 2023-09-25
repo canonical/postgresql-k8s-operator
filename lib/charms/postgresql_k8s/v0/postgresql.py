3edddb5c9f3c2f349d6d2cd8aebff12c727111ff
Dragomir Penev <6687393+dragomirp@users.noreply.github.com>
2023-09-25 18:54:06 +0300

    Update lib (#264)

--- lib/charms/postgresql_k8s/v0/postgresql.py
+++ lib/charms/postgresql_k8s/v0/postgresql.py
@@ -32,7 +32,7 @@ LIBAPI = 0
 
 # Increment this PATCH version before using `charmcraft publish-lib` or reset
 # to 0 if you are raising the major API version
-LIBPATCH = 13
+LIBPATCH = 14
 
 INVALID_EXTRA_USER_ROLE_BLOCKING_MESSAGE = "invalid role(s) for extra user roles"
 
