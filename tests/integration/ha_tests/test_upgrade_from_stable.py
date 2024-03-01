2032a825fbe67d289389416a4eb852d2b8672245
Carl Csaposs <carl.csaposs@canonical.com>
2024-03-01 15:15:25 +0100

    Fix `upstream-sources` usage

--- tests/integration/ha_tests/test_upgrade_from_stable.py
+++ tests/integration/ha_tests/test_upgrade_from_stable.py
@@ -102,7 +102,9 @@ async def test_upgrade_from_stable(ops_test: OpsTest, continuous_writes):
     primary_name = await get_primary(ops_test, DATABASE_APP_NAME)
     initial_number_of_switchovers = await count_switchovers(ops_test, primary_name)
 
-    resources = {"postgresql-image": METADATA["resources"]["postgresql-image"]["upstream-source"]}
+    resources = {
+        "postgresql-image": METADATA["resources"]["postgresql-image"]["upstream-sources"]["amd64"]
+    }
     application = ops_test.model.applications[DATABASE_APP_NAME]
     actions = await application.get_actions()
 
