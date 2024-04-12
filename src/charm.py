9e4c16c8b5c72b88514e9bb80dc1ace7e803c941
Lucas Gameiro <lucas.borges@canonical.com>
2024-04-12 08:58:47 -0300

    [DPE-3885] Enhance the error message if charm was deployed without `--trust` flag (#440)
    * improve error message for trust flag
    
    * fix message text
    
    * fix docstring
    
    * fix explanation
    
    * add integration test for new trust behavior
    
    * test different app name for deploy
    
    * fix bug
    
    * enable rbac from inside test
    
    * fix typo
    
    * use sudo with microk8s enable
    
    * remove check true
    
    * fix linting
    
    * test subprocess call
    
    * remove fast-forward
    
    * test connectivity and enable rbac
    
    * fix integration test + update endpoints
    
    * revert poetry lock change
    
    * reposition endpoint update in event
    
    * reposition cleanup resource call

--- src/charm.py
+++ src/charm.py
@@ -625,17 +625,19 @@ class PostgresqlOperatorCharm(TypedCharmBase[CharmConfig]):
 
     def _on_leader_elected(self, event: LeaderElectedEvent) -> None:
         """Handle the leader-elected event."""
-        if self.get_secret(APP_SCOPE, USER_PASSWORD_KEY) is None:
-            self.set_secret(APP_SCOPE, USER_PASSWORD_KEY, new_password())
+        for password in {
+            USER_PASSWORD_KEY,
+            REPLICATION_PASSWORD_KEY,
+            REWIND_PASSWORD_KEY,
+            MONITORING_PASSWORD_KEY,
+        }:
+            if self.get_secret(APP_SCOPE, password) is None:
+                self.set_secret(APP_SCOPE, password, new_password())
 
-        if self.get_secret(APP_SCOPE, REPLICATION_PASSWORD_KEY) is None:
-            self.set_secret(APP_SCOPE, REPLICATION_PASSWORD_KEY, new_password())
-
-        if self.get_secret(APP_SCOPE, REWIND_PASSWORD_KEY) is None:
-            self.set_secret(APP_SCOPE, REWIND_PASSWORD_KEY, new_password())
-
-        if self.get_secret(APP_SCOPE, MONITORING_PASSWORD_KEY) is None:
-            self.set_secret(APP_SCOPE, MONITORING_PASSWORD_KEY, new_password())
+        # Add this unit to the list of cluster members
+        # (the cluster should start with only this member).
+        if self._endpoint not in self._endpoints:
+            self._add_to_endpoints(self._endpoint)
 
         self._cleanup_old_cluster_resources()
         client = Client()
@@ -652,6 +654,9 @@ class PostgresqlOperatorCharm(TypedCharmBase[CharmConfig]):
                 )
                 self.app_peer_data.pop("cluster_initialised", None)
         except ApiError as e:
+            if e.status.code == 403:
+                self.on_deployed_without_trust()
+                return
             # Ignore the error only when the resource doesn't exist.
             if e.status.code != 404:
                 raise e
@@ -663,11 +668,6 @@ class PostgresqlOperatorCharm(TypedCharmBase[CharmConfig]):
             logger.exception("failed to create k8s services")
             self.unit.status = BlockedStatus("failed to create k8s services")
             return
-
-        # Add this unit to the list of cluster members
-        # (the cluster should start with only this member).
-        if self._endpoint not in self._endpoints:
-            self._add_to_endpoints(self._endpoint)
 
         # Remove departing units when the leader changes.
         self._remove_from_endpoints(self._get_endpoints_to_remove())
@@ -903,6 +903,9 @@ class PostgresqlOperatorCharm(TypedCharmBase[CharmConfig]):
                 )
                 logger.info(f"deleted {kind.__name__}/{self.cluster_name}{suffix}")
             except ApiError as e:
+                if e.status.code == 403:
+                    self.on_deployed_without_trust()
+                    return
                 # Ignore the error only when the resource doesn't exist.
                 if e.status.code != 404:
                     raise e
@@ -1398,7 +1401,14 @@ class PostgresqlOperatorCharm(TypedCharmBase[CharmConfig]):
             limit_memory = self.config.profile_limit_memory * 10**6
         else:
             limit_memory = None
-        available_cpu_cores, available_memory = self.get_available_resources()
+        try:
+            available_cpu_cores, available_memory = self.get_available_resources()
+        except ApiError as e:
+            if e.status.code == 403:
+                self.on_deployed_without_trust()
+                return
+            raise e
+
         postgresql_parameters = self.postgresql.build_postgresql_parameters(
             self.model.config, available_memory, limit_memory
         )
@@ -1593,6 +1603,18 @@ class PostgresqlOperatorCharm(TypedCharmBase[CharmConfig]):
                 allocable_memory = constrained_memory
 
         return cpu_cores, allocable_memory
+
+    def on_deployed_without_trust(self) -> None:
+        """Blocks the application and returns a specific error message for deployments made without --trust."""
+        self.unit.status = BlockedStatus(
+            f"Insufficient permissions, try: `juju trust {self._name} --scope=cluster`"
+        )
+        logger.error(
+            f"""
+            Access to k8s cluster resources is not authorized. This happens when RBAC is enabled and the deployed application was not trusted by the juju admin.
+            To fix this issue, run `juju trust {self._name} --scope=cluster` (or remove & re-deploy {self._name} with `--trust`)
+            """
+        )
 
     @property
     def client_relations(self) -> List[Relation]:
