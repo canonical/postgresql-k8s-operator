09f782401cb7e50c045f64c060e4399e09c8ea3c
Lucas Gameiro <lucas.borges@canonical.com>
2024-05-02 09:35:00 -0300

    [MISC] Update data-platform-libs: data_interfaces to 34 and upgrade to 16 (#454)
    * update libs
    
    * fix unit tests
    
    * add peer check
    
    * adapt src code and unit tests
    
    * separate tests in 2 versions
    
    * review suggestions
    
    * add new review suggestions

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -11,3 +11,20 @@ import pytest
 def juju_has_secrets(request, monkeypatch):
     monkeypatch.setattr("charm.JujuVersion.has_secrets", PropertyMock(return_value=request.param))
     return request.param
+
+
+@pytest.fixture
+def only_with_juju_secrets(juju_has_secrets):
+    """Pretty way to skip Juju 3 tests."""
+    if not juju_has_secrets:
+        pytest.skip("Secrets test only applies on Juju 3.x")
+
+
+@pytest.fixture
+def only_without_juju_secrets(juju_has_secrets):
+    """Pretty way to skip Juju 2-specific tests.
+
+    Typically: to save CI time, when the same check were executed in a Juju 3-specific way already
+    """
+    if juju_has_secrets:
+        pytest.skip("Skipping legacy secrets tests")
