b82cbac6de4f13617fca429c3db667a362029ea8
Carl Csaposs <carl.csaposs@canonical.com>
2024-05-17 13:38:43 +0200

    format

--- tests/integration/markers.py
+++ tests/integration/markers.py
@@ -4,8 +4,8 @@
 
 import pytest
 
-from .juju_ import juju_major_version
 from . import architecture
+from .juju_ import juju_major_version
 
 juju2 = pytest.mark.skipif(juju_major_version != 2, reason="Requires juju 2")
 juju3 = pytest.mark.skipif(juju_major_version != 3, reason="Requires juju 3")
