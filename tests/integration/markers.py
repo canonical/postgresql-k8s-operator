fda8b1a598bc2f6e3ba853acab39696ed8363bb1
Carl Csaposs <carl.csaposs@canonical.com>
2024-05-17 11:25:45 +0200

    Run integration tests on arm64

--- tests/integration/markers.py
+++ tests/integration/markers.py
@@ -5,7 +5,14 @@
 import pytest
 
 from .juju_ import juju_major_version
+from . import architecture
 
 juju2 = pytest.mark.skipif(juju_major_version != 2, reason="Requires juju 2")
 juju3 = pytest.mark.skipif(juju_major_version != 3, reason="Requires juju 3")
 juju_secrets = pytest.mark.skipif(juju_major_version < 3, reason="Requires juju secrets")
+amd64_only = pytest.mark.skipif(
+    architecture.architecture != "amd64", reason="Requires amd64 architecture"
+)
+arm64_only = pytest.mark.skipif(
+    architecture.architecture != "arm64", reason="Requires arm64 architecture"
+)
