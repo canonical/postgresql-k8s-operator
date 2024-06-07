d17428991d04afdaffd48a5e92a9e3b7adf462a9
Lucas Gameiro Borges <lucas.borges@canonical.com>
2024-06-07 19:13:46 +0000

    update test_charm parameter

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -174,7 +174,7 @@ async def test_postgresql_parameters_change(ops_test: OpsTest) -> None:
     """Test that's possible to change PostgreSQL parameters."""
     await ops_test.model.applications[APP_NAME].set_config({
         "memory_max_prepared_transactions": "100",
-        "memory_shared_buffers": "128",
+        "memory_shared_buffers": "32768",  # 2 * 128MB. Patroni may refuse the config if < 128MB
         "response_lc_monetary": "en_GB.utf8",
     })
     await ops_test.model.wait_for_idle(apps=[APP_NAME], status="active", idle_period=30)
@@ -200,7 +200,7 @@ async def test_postgresql_parameters_change(ops_test: OpsTest) -> None:
 
                 # Validate each configuration set by Patroni on PostgreSQL.
                 assert settings["max_prepared_transactions"] == "100"
-                assert settings["shared_buffers"] == "128"
+                assert settings["shared_buffers"] == "32768"
                 assert settings["lc_monetary"] == "en_GB.utf8"
         finally:
             connection.close()
