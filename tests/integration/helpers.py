00a4bc69cbe924c501fd60eccf31cb03d1b2a730
Marcelo Henrique Neppel <marcelo.neppel@canonical.com>
2024-06-11 17:44:21 -0300

    Fix backups integration test
    Signed-off-by: Marcelo Henrique Neppel <marcelo.neppel@canonical.com>

--- tests/integration/helpers.py
+++ tests/integration/helpers.py
@@ -761,10 +761,10 @@ async def wait_for_idle_on_blocked(
     unit = ops_test.model.units.get(f"{database_app_name}/{unit_number}")
     await asyncio.gather(
         ops_test.model.wait_for_idle(apps=[other_app_name], status="active"),
-        ops_test.model.wait_for_idle(
-            apps=[database_app_name], status="blocked", raise_on_blocked=False
+        ops_test.model.block_until(
+            lambda: unit.workload_status == "blocked"
+            and unit.workload_status_message == status_message
         ),
-        ops_test.model.block_until(lambda: unit.workload_status_message == status_message),
     )
 
 
