a627e0bed5c48bb23391a997a254060762a212fe
Marcelo Henrique Neppel <marcelo.neppel@canonical.com>
2024-06-13 00:21:14 -0300

    [DPE-4256] Async replication UX improvements (#491)

--- tests/integration/ha_tests/test_async_replication.py
+++ tests/integration/ha_tests/test_async_replication.py
@@ -140,10 +140,10 @@ async def test_async_replication(
     logger.info("checking whether writes are increasing")
     await are_writes_increasing(ops_test)
 
-    first_offer_command = f"offer {DATABASE_APP_NAME}:async-primary async-primary"
+    first_offer_command = f"offer {DATABASE_APP_NAME}:replication-offer replication-offer"
     await ops_test.juju(*first_offer_command.split())
     first_consume_command = (
-        f"consume -m {second_model.info.name} admin/{first_model.info.name}.async-primary"
+        f"consume -m {second_model.info.name} admin/{first_model.info.name}.replication-offer"
     )
     await ops_test.juju(*first_consume_command.split())
 
@@ -157,7 +157,7 @@ async def test_async_replication(
             ),
         )
 
-    await second_model.relate(DATABASE_APP_NAME, "async-primary")
+    await second_model.relate(DATABASE_APP_NAME, "replication-offer")
 
     async with ops_test.fast_forward(FAST_INTERVAL), fast_forward(second_model, FAST_INTERVAL):
         await gather(
@@ -177,7 +177,7 @@ async def test_async_replication(
     leader_unit = await get_leader_unit(ops_test, DATABASE_APP_NAME)
     assert leader_unit is not None, "No leader unit found"
     logger.info("promoting the first cluster")
-    run_action = await leader_unit.run_action("promote-cluster")
+    run_action = await leader_unit.run_action("create-replication")
     await run_action.wait()
     assert (run_action.results.get("return-code", None) == 0) or (
         run_action.results.get("Code", None) == "0"
@@ -212,10 +212,10 @@ async def test_switchover(
     second_model_continuous_writes,
 ):
     """Test switching over to the second cluster."""
-    second_offer_command = f"offer {DATABASE_APP_NAME}:async-replica async-replica"
+    second_offer_command = f"offer {DATABASE_APP_NAME}:replication replication"
     await ops_test.juju(*second_offer_command.split())
     second_consume_command = (
-        f"consume -m {second_model.info.name} admin/{first_model.info.name}.async-replica"
+        f"consume -m {second_model.info.name} admin/{first_model.info.name}.replication"
     )
     await ops_test.juju(*second_consume_command.split())
 
@@ -234,7 +234,7 @@ async def test_switchover(
     leader_unit = await get_leader_unit(ops_test, DATABASE_APP_NAME, model=second_model)
     assert leader_unit is not None, "No leader unit found"
     logger.info("promoting the second cluster")
-    run_action = await leader_unit.run_action("promote-cluster", **{"force-promotion": True})
+    run_action = await leader_unit.run_action("promote-to-primary", **{"force": True})
     await run_action.wait()
     assert (run_action.results.get("return-code", None) == 0) or (
         run_action.results.get("Code", None) == "0"
@@ -269,16 +269,16 @@ async def test_promote_standby(
     """Test promoting the standby cluster."""
     logger.info("breaking the relation")
     await second_model.applications[DATABASE_APP_NAME].remove_relation(
-        "async-replica", "async-primary"
+        "replication", "replication-offer"
     )
-    wait_for_relation_removed_between(ops_test, "async-primary", "async-replica", second_model)
+    wait_for_relation_removed_between(ops_test, "replication-offer", "replication", second_model)
     async with ops_test.fast_forward(FAST_INTERVAL), fast_forward(second_model, FAST_INTERVAL):
         await gather(
             first_model.wait_for_idle(
-                apps=[DATABASE_APP_NAME],
-                status="blocked",
-                idle_period=IDLE_PERIOD,
-                timeout=TIMEOUT,
+                apps=[DATABASE_APP_NAME], idle_period=IDLE_PERIOD, timeout=TIMEOUT
+            ),
+            first_model.block_until(
+                lambda: first_model.applications[DATABASE_APP_NAME].status == "blocked",
             ),
             second_model.wait_for_idle(
                 apps=[DATABASE_APP_NAME], status="active", idle_period=IDLE_PERIOD, timeout=TIMEOUT
@@ -290,7 +290,7 @@ async def test_promote_standby(
     leader_unit = await get_leader_unit(ops_test, DATABASE_APP_NAME)
     assert leader_unit is not None, "No leader unit found"
     logger.info("promoting the first cluster")
-    run_action = await leader_unit.run_action("promote-cluster")
+    run_action = await leader_unit.run_action("promote-to-primary")
     await run_action.wait()
     assert (run_action.results.get("return-code", None) == 0) or (
         run_action.results.get("Code", None) == "0"
@@ -346,7 +346,7 @@ async def test_reestablish_relation(
     await are_writes_increasing(ops_test)
 
     logger.info("reestablishing the relation")
-    await second_model.relate(DATABASE_APP_NAME, "async-primary")
+    await second_model.relate(DATABASE_APP_NAME, "replication-offer")
     async with ops_test.fast_forward(FAST_INTERVAL), fast_forward(second_model, FAST_INTERVAL):
         await gather(
             first_model.wait_for_idle(
@@ -365,7 +365,7 @@ async def test_reestablish_relation(
     leader_unit = await get_leader_unit(ops_test, DATABASE_APP_NAME)
     assert leader_unit is not None, "No leader unit found"
     logger.info("promoting the first cluster")
-    run_action = await leader_unit.run_action("promote-cluster")
+    run_action = await leader_unit.run_action("create-replication")
     await run_action.wait()
     assert (run_action.results.get("return-code", None) == 0) or (
         run_action.results.get("Code", None) == "0"
