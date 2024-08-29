eced2cea1b63c8e9c23f34ae9c18d5241e5a0037
Dragomir Penev <dragomir.penev@canonical.com>
2024-08-29 18:32:06 +0300

    Revert "Enable arm upgrade from stable"
    This reverts commit 9aa55aedee23dc5f3c04f5e4395104e90b09dc26.

--- tests/integration/ha_tests/test_upgrade_from_stable.py
+++ tests/integration/ha_tests/test_upgrade_from_stable.py
@@ -32,6 +32,7 @@ TIMEOUT = 5 * 60
 
 
 @pytest.mark.group(1)
+@markers.amd64_only  # TODO: remove after arm64 stable release
 @pytest.mark.abort_on_fail
 async def test_deploy_stable(ops_test: OpsTest) -> None:
     """Simple test to ensure that the PostgreSQL and application charms get deployed."""
@@ -57,6 +58,7 @@ async def test_deploy_stable(ops_test: OpsTest) -> None:
 
 
 @pytest.mark.group(1)
+@markers.amd64_only  # TODO: remove after arm64 stable release
 @pytest.mark.abort_on_fail
 async def test_pre_upgrade_check(ops_test: OpsTest) -> None:
     """Test that the pre-upgrade-check action runs successfully."""
@@ -89,6 +91,7 @@ async def test_pre_upgrade_check(ops_test: OpsTest) -> None:
 
 
 @pytest.mark.group(1)
+@markers.amd64_only  # TODO: remove after arm64 stable release
 @pytest.mark.abort_on_fail
 async def test_upgrade_from_stable(ops_test: OpsTest, continuous_writes):
     """Test updating from stable channel."""
