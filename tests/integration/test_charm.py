7bcadffcd6c77c19977ba18c693256dd90f16fa4
shayancanonical <99665202+shayancanonical@users.noreply.github.com>
2024-11-15 14:48:38 -0500

    [DPE-4977] Run integration tests against Juju 3.6 (#689)
    * Attempt to run integration tests on juju 3.6
    
    * Change pgdata permissions to 0o700 + fix charm bases in tests
    
    * Remove import to fix lint warning
    
    * Set pg data permissions to 750 explicitly so it works well with pgbackrest
    
    * Use base ubuntu@20.04 for finos-waltz-k8s
    
    * Deploy charms without jammy support with base ubuntu@20.04
    
    * Test against juju 3.6/edge due to secrets related fixes + deploy mattermost with ubuntu@20.04
    
    * Run tests against juju 3.6/stable
    
    * Run tests against 3.6/candidate and not 3.6/stable
    
    * Run juju 3.6 tests only on a nightly schedule
    
    * Address missed case of series->base after merge of main into branch
    
    * Address Dragomir's feedback on test failures due to nuances of python libjuju
    
    * Use series for the indico charm as the usage of bases shows incompatibility with focal

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -14,7 +14,7 @@ from pytest_operator.plugin import OpsTest
 from tenacity import Retrying, stop_after_delay, wait_fixed
 
 from .helpers import (
-    CHARM_SERIES,
+    CHARM_BASE,
     METADATA,
     STORAGE_PATH,
     build_and_deploy,
@@ -388,7 +388,7 @@ async def test_redeploy_charm_same_model(ops_test: OpsTest):
             },
             application_name=APP_NAME,
             num_units=len(UNIT_IDS),
-            series=CHARM_SERIES,
+            base=CHARM_BASE,
             trust=True,
             config={"profile": "testing"},
         )
@@ -432,7 +432,7 @@ async def test_redeploy_charm_same_model_after_forcing_removal(ops_test: OpsTest
             },
             application_name=APP_NAME,
             num_units=len(UNIT_IDS),
-            series=CHARM_SERIES,
+            base=CHARM_BASE,
             trust=True,
             config={"profile": "testing"},
         )
@@ -464,7 +464,7 @@ async def test_storage_with_more_restrictive_permissions(ops_test: OpsTest):
                 },
                 application_name=app_name,
                 num_units=1,
-                series=CHARM_SERIES,
+                base=CHARM_BASE,
                 trust=True,
                 config={"profile": "testing"},
             )
