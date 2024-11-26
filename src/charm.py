485fe58c69ccc81188d13c7edc55058748c00535
Lucas Gameiro Borges <lucas.borges@canonical.com>
2024-11-26 21:07:37 +0000

    remove argument on helper

--- src/charm.py
+++ src/charm.py
@@ -1879,7 +1879,7 @@ class PostgresqlOperatorCharm(TypedCharmBase[CharmConfig]):
                 logger.debug(
                     "Early exit update_config: patroni not responding but TLS is enabled."
                 )
-                self._handle_postgresql_restart_need(True)
+                self._handle_postgresql_restart_need()
                 return True
             logger.debug("Early exit update_config: Patroni not started yet")
             return False
