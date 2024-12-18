229a3ef4cba70838a42c137272d6de4d5f5bd1de
Sinclert Pérez <sinclert.perez@canonical.com>
2024-12-18 09:58:11 +0100

    Update data-platform-libs to use v1

--- src/config.py
+++ src/config.py
@@ -7,7 +7,7 @@
 import logging
 from typing import Literal
 
-from charms.data_platform_libs.v0.data_models import BaseConfigModel
+from charms.data_platform_libs.v1.data_models import BaseConfigModel
 from pydantic import PositiveInt, validator
 
 logger = logging.getLogger(__name__)
