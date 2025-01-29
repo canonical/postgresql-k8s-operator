eb80401d9a2de254e8ba70bb5035eadead0733d9
Sinclert Pérez <sinclert.perez@canonical.com>
2025-01-29 11:39:43 +0100

    draft 6 [tests]

--- tests/unit/test_postgresql.py
+++ tests/unit/test_postgresql.py
@@ -5,6 +5,8 @@ from unittest.mock import call, patch
 import psycopg2
 import pytest
 from charms.postgresql_k8s.v0.postgresql import (
+    ACCESS_GROUP_INTERNAL,
+    ACCESS_GROUP_RELATION,
     PERMISSIONS_GROUP_ADMIN,
     PostgreSQLCreateDatabaseError,
     PostgreSQLGetLastArchivedWALError,
@@ -19,6 +21,7 @@ from constants import (
     PEER,
     REPLICATION_USER,
     REWIND_USER,
+    SYSTEM_USERS,
     USER,
 )
 
@@ -163,6 +166,31 @@ def test_create_database(harness):
         except PostgreSQLCreateDatabaseError:
             pass
         _enable_disable_extensions.assert_not_called()
+
+
+def test_create_access_groups(harness):
+    with patch(
+        "charms.postgresql_k8s.v0.postgresql.PostgreSQL._connect_to_database"
+    ) as _connect_to_database:
+        execute = _connect_to_database.return_value.__enter__.return_value.cursor.return_value.__enter__.return_value.execute
+        harness.charm.postgresql.create_access_groups()
+
+        execute.assert_has_calls([
+            call(f"CREATE ROLE {ACCESS_GROUP_INTERNAL} NOLOGIN;"),
+            call(f"CREATE ROLE {ACCESS_GROUP_RELATION} NOLOGIN;"),
+            *(
+                call(
+                    Composed([
+                        SQL("GRANT "),
+                        Identifier(ACCESS_GROUP_INTERNAL),
+                        SQL(" TO "),
+                        Identifier(user),
+                        SQL(";"),
+                    ])
+                )
+                for user in SYSTEM_USERS
+            ),
+        ])
 
 
 def test_generate_database_privileges_statements(harness):
