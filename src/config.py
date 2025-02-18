a1e1131de7faa208ffdfcde0e769d5a8fecd4bd1
Danylo Bereznenko <pepsiqqfanta@gmail.com>
2025-02-18 10:55:14 +0200

    config improvement

--- src/config.py
+++ src/config.py
@@ -104,7 +104,6 @@ class CharmConfig(BaseConfigModel):
     storage_bgwriter_lru_maxpages: int | None
     storage_bgwriter_lru_multiplier: float | None
     response_bytea_output: str | None
-    request_client_encoding: str | None
     logging_client_min_messages: str | None
     optimizer_constraint_exclusion: str | None
     optimizer_cpu_index_tuple_cost: float | None
@@ -454,96 +453,6 @@ class CharmConfig(BaseConfigModel):
         """Check response_bytea_output config option is one of `escape` or `hex`."""
         if value not in ["escape", "hex"]:
             raise ValueError("Value not one of 'escape' or 'hex'")
-
-        return value
-
-    @validator("request_client_encoding")
-    @classmethod
-    def request_client_encoding_values(cls, value: str) -> str | None:
-        """Check request_client_encoding config option is one of BIG5, WIN950, Windows950, EUC_CN, EUC_JP, EUC_JIS_2004, EUC_KR, EUC_TW, GB18030, GBK, WIN936, Windows936, ISO_8859_5, ISO_8859_6, ISO_8859_7, ISO_8859_8, JOHAB, KOI8R, KOI8, KOI8U, LATIN1, ISO88591, LATIN2, ISO88592, LATIN3, ISO88593, LATIN4, ISO88594, LATIN5, ISO88599, LATIN6, ISO885910, LATIN7, ISO885913, LATIN8, ISO885914, LATIN9, ISO885915, LATIN10, ISO885916, MULE_INTERNAL, SJIS, Mskanji, ShiftJIS, WIN932, Windows932, SHIFT_JIS_2004, SQL_ASCII, UHC, WIN949, Windows949, UTF8, Unicode, WIN866, ALT, WIN874, WIN1250, WIN1251, WIN, WIN1252, WIN1253, WIN1254, WIN1255, WIN1256, WIN1257, WIN1258, ABC, TCVN, TCVN5712, VSCII."""
-        if value not in [
-            "BIG5",
-            "WIN950",
-            "Windows950",
-            "EUC_CN",
-            "EUC_JP",
-            "EUC_JIS_2004",
-            "EUC_KR",
-            "EUC_TW",
-            "GB18030",
-            "GBK",
-            "WIN936",
-            "Windows936",
-            "ISO_8859_5",
-            "ISO_8859_6",
-            "ISO_8859_7",
-            "ISO_8859_8",
-            "JOHAB",
-            "KOI8R",
-            "KOI8",
-            "KOI8U",
-            "LATIN1",
-            "ISO88591",
-            "LATIN2",
-            "ISO88592",
-            "LATIN3",
-            "ISO88593",
-            "LATIN4",
-            "ISO88594",
-            "LATIN5",
-            "ISO88599",
-            "LATIN6",
-            "ISO885910",
-            "LATIN7",
-            "ISO885913",
-            "LATIN8",
-            "ISO885914",
-            "LATIN9",
-            "ISO885915",
-            "LATIN10",
-            "ISO885916",
-            "MULE_INTERNAL",
-            "SJIS",
-            "Mskanji",
-            "ShiftJIS",
-            "WIN932",
-            "Windows932",
-            "SHIFT_JIS_2004",
-            "SQL_ASCII",
-            "UHC",
-            "WIN949",
-            "Windows949",
-            "UTF8",
-            "Unicode",
-            "WIN866",
-            "ALT",
-            "WIN874",
-            "WIN1250",
-            "WIN1251",
-            "WIN",
-            "WIN1252",
-            "WIN1253",
-            "WIN1254",
-            "WIN1255",
-            "WIN1256",
-            "WIN1257",
-            "WIN1258",
-            "ABC",
-            "TCVN",
-            "TCVN5712",
-            "VSCII",
-        ]:
-            raise ValueError(
-                "Value not one of BIG5, WIN950, Windows950, EUC_CN, EUC_JP, EUC_JIS_2004, "
-                "EUC_KR, EUC_TW, GB18030, GBK, WIN936, Windows936, ISO_8859_5, ISO_8859_6, "
-                "ISO_8859_7, ISO_8859_8, JOHAB, KOI8R, KOI8, KOI8U, LATIN1, ISO88591, LATIN2, "
-                "ISO88592, LATIN3, ISO88593, LATIN4, ISO88594, LATIN5, ISO88599, LATIN6, "
-                "ISO885910, LATIN7, ISO885913, LATIN8, ISO885914, LATIN9, ISO885915, LATIN10, "
-                "ISO885916, MULE_INTERNAL, SJIS, Mskanji, ShiftJIS, WIN932, Windows932, SHIFT_JIS_2004, "
-                "SQL_ASCII, UHC, WIN949, Windows949, UTF8, Unicode, WIN866, ALT, WIN874, "
-                "WIN1250, WIN1251, WIN, WIN1252, WIN1253, WIN1254, WIN1255, WIN1256, WIN1257, "
-                "WIN1258, ABC, TCVN, TCVN5712, VSCII."
-            )
 
         return value
 
