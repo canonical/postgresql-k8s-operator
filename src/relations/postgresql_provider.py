71fb9d499de9800d981f293b6647662066cdd095
Sinclert Pérez <sinclert.perez@canonical.com>
2025-03-05 16:43:50 +0100

    LDAP I: Define and assign access groups

--- src/relations/postgresql_provider.py
+++ src/relations/postgresql_provider.py
@@ -10,6 +10,8 @@ from charms.data_platform_libs.v0.data_interfaces import (
     DatabaseRequestedEvent,
 )
 from charms.postgresql_k8s.v0.postgresql import (
+    ACCESS_GROUP_RELATION,
+    ACCESS_GROUPS,
     INVALID_EXTRA_USER_ROLE_BLOCKING_MESSAGE,
     PostgreSQLCreateDatabaseError,
     PostgreSQLCreateUserError,
@@ -71,7 +73,10 @@ class PostgreSQLProvider(Object):
         if extra_roles is None:
             return []
 
-        return [role.lower() for role in extra_roles.split(",")]
+        # Make sure the access-groups are not in the list
+        extra_roles_list = [role.lower() for role in extra_roles.split(",")]
+        extra_roles_list = [role for role in extra_roles_list if role not in ACCESS_GROUPS]
+        return extra_roles_list
 
     def _on_database_requested(self, event: DatabaseRequestedEvent) -> None:
         """Handle the legacy postgresql-client relation changed event.
@@ -89,8 +94,9 @@ class PostgreSQLProvider(Object):
         # Retrieve the database name and extra user roles using the charm library.
         database = event.database
 
-        # Make sure that certain groups are not in the list
+        # Make sure the relation access-group is added to the list
         extra_user_roles = self._sanitize_extra_roles(event.extra_user_roles)
+        extra_user_roles.append(ACCESS_GROUP_RELATION)
 
         try:
             # Creates the user and the database for this specific relation.
