ebf4756ed4e96330830021eb310a6a3c4e18cb79
Dragomir Penev <6687393+dragomirp@users.noreply.github.com>
2025-03-07 19:00:55 +0200

    [MISC] Skip backup tests without creds (#884)
    * Bump libs
    
    * Skip backups tests if no creds are set
    
    * Check that the cloud config is set to a value
    
    * Typo
    
    * Update tests/integration/conftest.py
    
    Co-authored-by: Carl Csaposs <carl.csaposs@canonical.com>
    
    * Switch all checks to gets
    
    * Typo
    
    ---------
    
    Co-authored-by: Carl Csaposs <carl.csaposs@canonical.com>

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,8 +1,20 @@
 # Copyright 2022 Canonical Ltd.
 # See LICENSE file for licensing details.
+import logging
+import os
+import uuid
+
+import boto3
 import pytest
+from pytest_operator.plugin import OpsTest
 
 from . import architecture
+from .helpers import construct_endpoint
+
+AWS = "AWS"
+GCP = "GCP"
+
+logger = logging.getLogger(__name__)
 
 
 @pytest.fixture(scope="session")
@@ -11,3 +23,74 @@ def charm():
     # juju bundle files expect local charms to begin with `./` or `/` to distinguish them from
     # Charmhub charms.
     return f"./postgresql-k8s_ubuntu@22.04-{architecture.architecture}.charm"
+
+
+def get_cloud_config(cloud: str) -> tuple[dict[str, str], dict[str, str]]:
+    # Define some configurations and credentials.
+    if cloud == AWS:
+        return {
+            "endpoint": "https://s3.amazonaws.com",
+            "bucket": "data-charms-testing",
+            "path": f"/postgresql-k8s/{uuid.uuid1()}",
+            "region": "us-east-1",
+        }, {
+            "access-key": os.environ["AWS_ACCESS_KEY"],
+            "secret-key": os.environ["AWS_SECRET_KEY"],
+        }
+    elif cloud == GCP:
+        return {
+            "endpoint": "https://storage.googleapis.com",
+            "bucket": "data-charms-testing",
+            "path": f"/postgresql-k8s/{uuid.uuid1()}",
+            "region": "",
+        }, {
+            "access-key": os.environ["GCP_ACCESS_KEY"],
+            "secret-key": os.environ["GCP_SECRET_KEY"],
+        }
+
+
+def cleanup_cloud(config: dict[str, str], credentials: dict[str, str]) -> None:
+    # Delete the previously created objects.
+    logger.info("deleting the previously created backups")
+    session = boto3.session.Session(
+        aws_access_key_id=credentials["access-key"],
+        aws_secret_access_key=credentials["secret-key"],
+        region_name=config["region"],
+    )
+    s3 = session.resource(
+        "s3", endpoint_url=construct_endpoint(config["endpoint"], config["region"])
+    )
+    bucket = s3.Bucket(config["bucket"])
+    # GCS doesn't support batch delete operation, so delete the objects one by one.
+    for bucket_object in bucket.objects.filter(Prefix=config["path"].lstrip("/")):
+        bucket_object.delete()
+
+
+@pytest.fixture(scope="module")
+async def aws_cloud_configs(ops_test: OpsTest) -> None:
+    if (
+        not os.environ.get("AWS_ACCESS_KEY", "").strip()
+        or not os.environ.get("AWS_SECRET_KEY", "").strip()
+    ):
+        pytest.skip("AWS configs not set")
+        return
+
+    config, credentials = get_cloud_config(AWS)
+    yield config, credentials
+
+    cleanup_cloud(config, credentials)
+
+
+@pytest.fixture(scope="module")
+async def gcp_cloud_configs(ops_test: OpsTest) -> None:
+    if (
+        not os.environ.get("GCP_ACCESS_KEY", "").strip()
+        or not os.environ.get("GCP_SECRET_KEY", "").strip()
+    ):
+        pytest.skip("GCP configs not set")
+        return
+
+    config, credentials = get_cloud_config(GCP)
+    yield config, credentials
+
+    cleanup_cloud(config, credentials)
