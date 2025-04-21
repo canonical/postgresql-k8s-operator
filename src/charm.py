b6420bfdc5ad7e8e5ca4e47699446feddd94ff72
Dragomir Penev <dragomir.penev@canonical.com>
2025-04-21 17:09:05 +0300

    Make username mandatory

--- src/charm.py
+++ src/charm.py
@@ -1240,7 +1240,9 @@ class PostgresqlOperatorCharm(TypedCharmBase[CharmConfig]):
 
         If no user is provided, the password of the operator user is returned.
         """
-        username = event.params.get("username", USER)
+        if not (username := event.params.get("username")):
+            event.fail("The action requires a username")
+            return
         if username not in PASSWORD_USERS and self.is_ldap_enabled:
             event.fail("The action can be run only for system users when LDAP is enabled")
             return
@@ -1260,7 +1262,9 @@ class PostgresqlOperatorCharm(TypedCharmBase[CharmConfig]):
             event.fail("The action can be run only on leader unit")
             return
 
-        username = event.params.get("username", USER)
+        if not (username := event.params.get("username")):
+            event.fail("The action requires a username")
+            return
         if username not in SYSTEM_USERS and self.is_ldap_enabled:
             event.fail("The action can be run only for system users when LDAP is enabled")
             return
