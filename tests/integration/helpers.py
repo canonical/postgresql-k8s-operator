73d366017c1323c31db08871b720089860ee7cce
René Radoi <rene.radoi@canonical.com>
2025-04-23 11:55:14 +0200

    [DPE-6189] Manage passwords with user secrets (#926)
    This PR introduces juju user secrets for managing passwords. It contains the following changes:
    
    **Functionality:**
    - no longer support `get-password` and `set-password` actions
    - new config option `system-users` to configure a secret that includes the system users' password(s)
    - it is no longer possible to leave out the `password` parameter to trigger a password rotation as secrets cannot have empty values
    
    **Implementation:**
    - add handler for `secret_changed` event to `charm.py`
    - add method `get_secret_from_id()` to `charm.py`
    - trigger updating the system user passwords on `config_changed`
    - consider pre-configured system user passwords on `leader_elected`
    - remove `get_password` handler
    - replace `set_password` handler with `_update_admin_passwords()` method
    - `_update_admin_passwords()` is responsible for the actual business logic:
      - retrieving the passwords from the configured secret
      - checking if password updates need to be performed
      - calling the `postgresql.update_user_password()` method
    
    **Testing:**
    - remove integration tests that are no longer required, such as testing for empty password or testing if the output of `get-password` is the same as the output of `juju show-secret`
    - adjust integration test helpers for `get_password()` and `set_password` to use secrets
    - remove unit tests for `get-/set-password` actions

--- tests/integration/helpers.py
+++ tests/integration/helpers.py
@@ -3,6 +3,7 @@
 # See LICENSE file for licensing details.
 import asyncio
 import itertools
+import json
 import logging
 from datetime import datetime
 from multiprocessing import ProcessError
@@ -32,7 +33,7 @@ from tenacity import (
     wait_fixed,
 )
 
-from constants import DATABASE_DEFAULT_NAME
+from constants import DATABASE_DEFAULT_NAME, PEER, SYSTEM_USERS_PASSWORD_CONFIG
 
 CHARM_BASE = "ubuntu@22.04"
 CHARM_BASE_NOBLE = "ubuntu@24.04"
@@ -40,6 +41,11 @@ METADATA = yaml.safe_load(Path("./metadata.yaml").read_text())
 DATABASE_APP_NAME = METADATA["name"]
 APPLICATION_NAME = "postgresql-test-app"
 STORAGE_PATH = METADATA["storage"]["pgdata"]["location"]
+
+
+class SecretNotFoundError(Exception):
+    """Raised when a secret is not found."""
+
 
 try:
     check_call(["kubectl", "version", "--client=true"])
@@ -445,28 +451,34 @@ async def get_leader_unit(ops_test: OpsTest, app: str, model: Model = None) -> U
     return leader_unit
 
 
-async def get_password_on_unit(
-    ops_test: OpsTest, username: str, unit: Unit, database_app_name: str = DATABASE_APP_NAME
-) -> str:
-    action = await unit.run_action("get-password", **{"username": username})
-    result = await action.wait()
-    return result.results["password"]
-
-
 async def get_password(
     ops_test: OpsTest,
     username: str = "operator",
     database_app_name: str = DATABASE_APP_NAME,
-    down_unit: str | None = None,
-    unit_name: str | None = None,
 ):
-    """Retrieve a user password using the action."""
-    for unit in ops_test.model.applications[database_app_name].units:
-        if unit.name == down_unit:
-            continue
+    """Retrieve a user password from the secret."""
+    secret = await get_secret_by_label(ops_test, label=f"{PEER}.{database_app_name}.app")
+    password = secret.get(f"{username}-password")
 
-        if pw := await get_password_on_unit(ops_test, username, unit, database_app_name):
-            return pw
+    return password
+
+
+async def get_secret_by_label(ops_test: OpsTest, label: str) -> dict[str, str]:
+    secrets_raw = await ops_test.juju("list-secrets")
+    secret_ids = [
+        secret_line.split()[0] for secret_line in secrets_raw[1].split("\n")[1:] if secret_line
+    ]
+
+    for secret_id in secret_ids:
+        secret_data_raw = await ops_test.juju(
+            "show-secret", "--format", "json", "--reveal", secret_id
+        )
+        secret_data = json.loads(secret_data_raw[1])
+
+        if label == secret_data[secret_id].get("label"):
+            return secret_data[secret_id]["content"]["Data"]
+
+    raise SecretNotFoundError(f"Secret with label {label} not found")
 
 
 @retry(
@@ -724,16 +736,28 @@ async def scale_application(
 
 
 async def set_password(
-    ops_test: OpsTest, unit_name: str, username: str = "operator", password: str | None = None
+    ops_test: OpsTest,
+    app_name: str = DATABASE_APP_NAME,
+    username: str = "operator",
+    password: str | None = None,
 ):
-    """Set a user password using the action."""
-    unit = ops_test.model.units.get(unit_name)
-    parameters = {"username": username}
-    if password is not None:
-        parameters["password"] = password
-    action = await unit.run_action("set-password", **parameters)
-    result = await action.wait()
-    return result.results
+    """Set a user password via secret."""
+    secret_name = "system_users_secret"
+
+    try:
+        secret_id = await ops_test.model.add_secret(
+            name=secret_name, data_args=[f"{username}={password}"]
+        )
+        await ops_test.model.grant_secret(secret_name=secret_name, application=app_name)
+
+        # update the application config to include the secret
+        await ops_test.model.applications[app_name].set_config({
+            SYSTEM_USERS_PASSWORD_CONFIG: secret_id
+        })
+    except Exception:
+        await ops_test.model.update_secret(
+            name=secret_name, data_args=[f"{username}={password}"], new_name=secret_name
+        )
 
 
 async def switchover(
--- tests/integration/test_password_rotation.py
+++ tests/integration/test_password_rotation.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 # Copyright 2022 Canonical Ltd.
 # See LICENSE file for licensing details.
-import json
 import re
 import time
 
@@ -9,13 +8,11 @@ import psycopg2
 import pytest
 from pytest_operator.plugin import OpsTest
 
-from . import markers
 from .helpers import (
     METADATA,
     build_and_deploy,
     check_patroni,
     db_connect,
-    get_leader_unit,
     get_password,
     get_primary,
     get_unit_address,
@@ -44,57 +41,40 @@ async def test_password_rotation(ops_test: OpsTest):
     backup_password = await get_password(ops_test, "backup")
     rewind_password = await get_password(ops_test, "rewind")
 
-    # Get the leader unit name (because passwords can only be set through it).
-    leader_unit = await get_leader_unit(ops_test, APP_NAME)
-    leader = leader_unit.name
-
     # Change both passwords.
-    result = await set_password(ops_test, unit_name=leader)
-    assert "password" in result
+    await set_password(ops_test, password="test-password")
     await ops_test.model.wait_for_idle(apps=[APP_NAME], status="active", timeout=1000)
+    new_superuser_password = await get_password(ops_test)
+    assert superuser_password != new_superuser_password
 
     # For replication, generate a specific password and pass it to the action.
     new_replication_password = "test-password"
-    result = await set_password(
-        ops_test, unit_name=leader, username="replication", password=new_replication_password
-    )
-    assert "password" in result
+    await set_password(ops_test, username="replication", password=new_replication_password)
     await ops_test.model.wait_for_idle(apps=[APP_NAME], status="active", timeout=1000)
+    assert new_replication_password == await get_password(ops_test, "replication")
+    assert replication_password != new_replication_password
 
     # For monitoring, generate a specific password and pass it to the action.
     new_monitoring_password = "test-password"
-    result = await set_password(
-        ops_test, unit_name=leader, username="monitoring", password=new_monitoring_password
-    )
-    assert "password" in result
+    await set_password(ops_test, username="monitoring", password=new_monitoring_password)
     await ops_test.model.wait_for_idle(apps=[APP_NAME], status="active", timeout=1000)
+    assert new_monitoring_password == await get_password(ops_test, "monitoring")
+    assert monitoring_password != new_monitoring_password
 
     # For backup, generate a specific password and pass it to the action.
     new_backup_password = "test-password"
-    result = await set_password(
-        ops_test, unit_name=leader, username="backup", password=new_backup_password
-    )
-    assert "password" in result
+    await set_password(ops_test, username="backup", password=new_backup_password)
     await ops_test.model.wait_for_idle(apps=[APP_NAME], status="active", timeout=1000)
+    assert new_backup_password == await get_password(ops_test, "backup")
+    assert backup_password != new_backup_password
 
     # For rewind, generate a specific password and pass it to the action.
     new_rewind_password = "test-password"
-    result = await set_password(
-        ops_test, unit_name=leader, username="rewind", password=new_rewind_password
-    )
-    assert "password" in result
+    await set_password(ops_test, username="rewind", password=new_rewind_password)
     await ops_test.model.wait_for_idle(apps=[APP_NAME], status="active", timeout=1000)
-
-    new_superuser_password = await get_password(ops_test)
-    assert superuser_password != new_superuser_password
-    assert new_replication_password == await get_password(ops_test, "replication")
-    assert replication_password != new_replication_password
-    assert new_monitoring_password == await get_password(ops_test, "monitoring")
-    assert monitoring_password != new_monitoring_password
-    assert new_backup_password == await get_password(ops_test, "backup")
-    assert backup_password != new_backup_password
     assert new_rewind_password == await get_password(ops_test, "rewind")
     assert rewind_password != new_rewind_password
+
     patroni_password = await get_password(ops_test, "patroni")
 
     # Restart Patroni on any non-leader unit and check that
@@ -104,42 +84,6 @@ async def test_password_rotation(ops_test: OpsTest):
         if not await unit.is_leader_from_status():
             await restart_patroni(ops_test, unit.name, patroni_password)
             assert await check_patroni(ops_test, unit.name, restart_time)
-
-
-@markers.juju_secrets
-async def test_password_from_secret_same_as_cli(ops_test: OpsTest):
-    """Checking if password is same as returned by CLI.
-
-    I.e. we're manipulating the secret we think we're manipulating.
-    """
-    #
-    # No way to retrieve a secret by label for now (https://bugs.launchpad.net/juju/+bug/2037104)
-    # Therefore we take advantage of the fact, that we only have ONE single secret at this point
-    # So we take the single member of the list
-    # NOTE: This would BREAK if for instance units had secrets at the start...
-    #
-    password = await get_password(ops_test, username="replication")
-    complete_command = "list-secrets"
-    _, stdout, _ = await ops_test.juju(*complete_command.split())
-    secret_id = stdout.split("\n")[1].split(" ")[0]
-
-    # Getting back the pw from juju CLI
-    complete_command = f"show-secret {secret_id} --reveal --format=json"
-    _, stdout, _ = await ops_test.juju(*complete_command.split())
-    data = json.loads(stdout)
-    assert data[secret_id]["content"]["Data"]["replication-password"] == password
-
-
-async def test_empty_password(ops_test: OpsTest) -> None:
-    """Test that the password can't be set to an empty string."""
-    leader_unit = await get_leader_unit(ops_test, APP_NAME)
-    leader = leader_unit.name
-    await set_password(ops_test, unit_name=leader, username="replication", password="")
-    password = await get_password(ops_test, unit_name=leader, username="replication")
-    # The password is 'None', BUT NOT because of SECRET_DELETED_LABEL
-    # `get_secret()` returns a None value (as the field in the secret is set to string value "None")
-    # And this true None value is turned to a string when the event is setting results.
-    assert password == "None"
 
 
 async def test_db_connection_with_empty_password(ops_test: OpsTest):
@@ -152,11 +96,9 @@ async def test_db_connection_with_empty_password(ops_test: OpsTest):
 
 async def test_no_password_change_on_invalid_password(ops_test: OpsTest) -> None:
     """Test that in general, there is no change when password validation fails."""
-    leader_unit = await get_leader_unit(ops_test, APP_NAME)
-    leader = leader_unit.name
     password1 = await get_password(ops_test, username="replication")
     # The password has to be minimum 3 characters
-    await set_password(ops_test, unit_name=leader, username="replication", password="ca" * 1000000)
+    await set_password(ops_test, username="replication", password="1")
     password2 = await get_password(ops_test, username="replication")
     # The password didn't change
     assert password1 == password2
