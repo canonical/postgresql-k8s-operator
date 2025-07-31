e5c11e7c26760e4c32315fce005cdc4556016f72
Dragomir Penev <dragomir.penev@canonical.com>
2025-07-31 17:47:04 +0300

    Add custom users to pg_hba filter

--- src/charm.py
+++ src/charm.py
@@ -2331,9 +2331,16 @@ class PostgresqlOperatorCharm(TypedCharmBase[CharmConfig]):
         ):
             return {USER: "all", REPLICATION_USER: "all", REWIND_USER: "all"}
         user_database_map = {}
-        for user in self.postgresql.list_users_from_relation(
-            current_host=self.is_connectivity_enabled
-        ):
+        for user in self.postgresql.list_users(current_host=self.is_connectivity_enabled):
+            if user in (
+                "backup",
+                "monitoring",
+                "operator",
+                "postgres",
+                "replication",
+                "rewind",
+            ):
+                continue
             user_database_map[user] = ",".join(
                 self.postgresql.list_accessible_databases_for_user(
                     user, current_host=self.is_connectivity_enabled
