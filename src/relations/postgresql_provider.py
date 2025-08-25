5a8c5b7c5d1793d4d6a55cafcb390b94fba35ac1
Marcelo Henrique Neppel <marcelo.neppel@canonical.com>
2025-08-25 08:41:01 -0300

    Fix check in relation initialisation (#1061)
    Signed-off-by: Marcelo Henrique Neppel <marcelo.neppel@canonical.com>

--- src/relations/postgresql_provider.py
+++ src/relations/postgresql_provider.py
@@ -77,7 +77,7 @@ class PostgreSQLProvider(Object):
         Generate password and handle user and database creation for the related application.
         """
         # Check for some conditions before trying to access the PostgreSQL instance.
-        if not self.charm.is_cluster_initialised or not self.charm._patroni.member_started:
+        if not self.charm.is_cluster_initialised or not self.charm._patroni.primary_endpoint_ready:
             logger.debug(
                 "Deferring on_database_requested: Cluster must be initialized before database can be requested"
             )
