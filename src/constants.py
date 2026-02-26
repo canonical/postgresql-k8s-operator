b7c9a7f448e97070c0d9e4c06836ae8ddbe7173d
Dragomir Penev <6687393+dragomirp@users.noreply.github.com>
2026-02-26 14:59:58 +0200

    [DPE-9444] Switch to ops tracing (14/edge) (#1279)
    * Switch to ops tracing
    
    * Bump protobuf
    
    * Try different tls transfer relation
    
    * Remove extra rel
    
    * Remove dep comment

--- src/constants.py
+++ src/constants.py
@@ -52,7 +52,6 @@ PLUGIN_OVERRIDES = {"audit": "pgaudit", "uuid_ossp": '"uuid-ossp"'}
 SPI_MODULE = ["refint", "autoinc", "insert_username", "moddatetime"]
 
 TRACING_RELATION_NAME = "tracing"
-TRACING_PROTOCOL = "otlp_http"
 
 DATABASE = "database"
 LEGACY_DB = "db"
