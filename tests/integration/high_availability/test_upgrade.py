2c3e6dccb148843017a505c1585745f9fc9cb603
Dragomir Penev <6687393+dragomirp@users.noreply.github.com>
2026-03-05 16:45:16 +0200

    [DPE-9479] Test app channel and base/series (16/edge) (#1325)
    * Test app channel and base/series
    
    * Switch from base to series
    
    * Linting
    
    * Switch base to series
    
    * Revert spread changes

--- tests/integration/high_availability/test_upgrade.py
+++ tests/integration/high_availability/test_upgrade.py
@@ -44,7 +44,7 @@ def test_deploy_latest(juju: Juju) -> None:
     juju.deploy(
         charm=DB_TEST_APP_NAME,
         app=DB_TEST_APP_NAME,
-        base="ubuntu@22.04",
+        base="ubuntu@24.04",
         channel="latest/edge",
         num_units=1,
     )
