4a778356dbd6812517bd53f342a36a54a71dc4e0
andreia <andreia.velasco@canonical.com>
2026-03-24 14:12:29 +0100

    add cookie banner

--- docs/conf.py
+++ docs/conf.py
@@ -207,9 +207,12 @@ sitemap_excludes = [
 # Template and asset locations
 #######################
 
-#html_static_path = ["_static"]
-#templates_path = ["_templates"]
-
+html_static_path = [
+    ".sphinx/_static",
+]
+templates_path = [
+    ".sphinx/_templates",
+]
 
 #############
 # Redirects #
@@ -306,15 +309,15 @@ extensions = [
 
 exclude_patterns = []
 
-# Adds custom CSS files, located under 'html_static_path'
+# Add CSS files (located in .sphinx/_static/)
+html_css_files = [
+    "cookie-banner.css",
+]
 
-# html_css_files = []
-
-
-# Adds custom JavaScript files, located under 'html_static_path'
-
-# html_js_files = []
-
+# Add JavaScript files (located in .sphinx/_static/)
+html_js_files = [
+    "bundle.js",
+]
 
 # Specifies a reST snippet to be appended to each .rst file
 
