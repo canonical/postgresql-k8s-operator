8668d6ae0a328d13b85a430871dd9a789efe48f5
Dragomir Penev <dragomir.penev@canonical.com>
2026-06-03 17:25:19 +0300

    Force python 3.10

--- tests/integration/helpers.py
+++ tests/integration/helpers.py
@@ -14,6 +14,7 @@ import botocore
 import psycopg2
 import requests
 import yaml
+from constants import DATABASE_DEFAULT_NAME, PEER, SYSTEM_USERS_PASSWORD_CONFIG
 from juju.model import Model
 from juju.unit import Unit
 from lightkube.core.client import Client
@@ -32,8 +33,6 @@ from tenacity import (
     wait_exponential,
     wait_fixed,
 )
-
-from constants import DATABASE_DEFAULT_NAME, PEER, SYSTEM_USERS_PASSWORD_CONFIG
 
 CHARM_BASE = "ubuntu@22.04"
 CHARM_BASE_NOBLE = "ubuntu@24.04"
